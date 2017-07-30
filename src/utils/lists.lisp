90d2b5a43e8a092c3ed8c30218b601fd0795e2a5
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-07-30 17:02:06 +0200

    remove compiler macro (for now).

--- src/utils/lists.lisp
+++ src/utils/lists.lisp
@@ -110,27 +110,3 @@
   (let ((r (try-find-cell item list :test test :key key)))
     (values (car r)
             (not (null r)))))
-
-
-(define-compiler-macro try-find (&whole form &environment env item list &key (test #'eql) (key #'identity))
-  (if (nor (symbolp test) (symbolp key))
-      (let-functions ((test eql) (key identity))
-        `(let ((r (try-find-cell ,item ,list :test ,test :key ,key)))
-           (values (car r)
-                   (not (null r)))))
-      form))
-
-
-(define-compiler-macro try-find-cell (&whole form &environment env item list &key (test #'eql) (key #'identity))
-  (if (nor (symbolp test) (symbolp key))
-      (let-functions ((test eql) (key identity))
-        `(iterate
-           (for elt on list)
-           (for val = (inlined-funcall ,key (car elt)))
-           (when (inlined-funcall ,test
-                                  val
-                                  item)
-             (leave elt))))
-      form))
-
-
