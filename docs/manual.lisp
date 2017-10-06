891470392a3cb5e5d8e76bc9eb11d23d9fda8cd2
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-10-06 20:09:46 +0200

    There is no point to have build-document.

--- docs/manual.lisp
+++ docs/manual.lisp
@@ -342,12 +342,3 @@
     @end{section}
 
     @end{section}))
-
-(defun build-document ()
-  (with-names (<documentation-names>)
-    (document (cl-lore.mechanics:<mechanics-html-output-generator> out *cl-data-structures*
-               :output-options (:css cl-lore.mechanics:*mechanics-html-style*))
-      @title{CL-DATA-STRUCTURES}
-      @include{cl-ds intro}
-      @include{cl-ds API}
-      @include{dicts})))
