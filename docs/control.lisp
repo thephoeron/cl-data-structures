7cfed26766324adf29f4748268957c069b9b8ae3
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-10-06 20:02:13 +0200

    Removed hardoced path to my /home from code.

--- docs/control.lisp
+++ docs/control.lisp
@@ -1,6 +1,3 @@
 (in-package #:cl-data-structures.documentation)
-(cl-lore.api.syntax:define-save-output-function save-output-impl)
 
-
-(defun save-output (&optional path)
-  (save-output-impl (build-document) path))
+(cl-lore.api.syntax:define-save-output-function save-output (build-document))
