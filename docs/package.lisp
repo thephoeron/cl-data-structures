c1d860667ae7c4eb815d3e76dd0431609138bc75
shka <sirherrbatka@gmail.com>
2017-10-07 12:22:52 +0200

    Move document options up.

--- docs/package.lisp
+++ docs/package.lisp
@@ -13,11 +13,12 @@
 
 (cl-lore.api.syntax:define-save-output-function
     build-docs
+    (cl-lore.mechanics:<mechanics-html-output-generator>
+     *cl-data-structures*
+     :output-options (:css cl-lore.mechanics:*mechanics-html-style*))
     ("manual.lisp")
     (with-names (<documentation-names>)
-      (document (cl-lore.mechanics:<mechanics-html-output-generator> out *cl-data-structures*
-                 :output-options (:css cl-lore.mechanics:*mechanics-html-style*))
-        @title{CL-DATA-STRUCTURES}
-        @include{cl-ds intro}
-        @include{cl-ds API}
-        @include{dicts})))
+      @title{CL-DATA-STRUCTURES}
+      @include{cl-ds intro}
+      @include{cl-ds API}
+      @include{dicts}))
