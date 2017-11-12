ee9de6a9c308403ba80dcb83d49055dc1e044288
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-11-12 16:01:20 +0100

    Updated to follow cl-lore changes.

--- docs/package.lisp
+++ docs/package.lisp
@@ -9,14 +9,11 @@
  cl-lore.extensions.documentation.api
  cl-lore.extensions.sequence-graphs.api)
 
-
 (cl-lore.api.syntax:define-save-output-function
     build-docs
-
-    ((<documentation-names>)
-     cl-lore.mechanics:<mechanics-html-output-generator>
-     *cl-data-structures*
-     :output-options (:css cl-lore.mechanics:*mechanics-html-style*))
+    ((<documentation-names>) cl-lore.mechanics:<mechanics-html-output-generator> *cl-data-structures*)
+    (:output-options (:css cl-lore.mechanics:*mechanics-html-style*)
+     :dynamic-binding ((cl-lore.extensions.documentation.protocol:*index* cl-data-structures:*documentation*)))
 
   ("vars.lisp"
    "manual.lisp"
