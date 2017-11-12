f66bf884f145c50317940a7d6925a1c24ff6ee62
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-11-12 12:47:28 +0100

    Added file that runs tests in the console.

---
+++ run-tests.lisp
@@ -0,0 +1,10 @@
+(ql:quickload :cl-data-structures)
+
+(let ((*error-output* (make-broadcast-stream)))
+  (handler-bind
+      ((lparallel.kernel:no-kernel-error
+         (lambda (c)
+           (declare (ignore c))
+           (invoke-restart 'lparallel.kernel:make-kernel 4))))
+    (prove:run :cl-data-structures :reporter :dot)))
+(cl-user::quit)
