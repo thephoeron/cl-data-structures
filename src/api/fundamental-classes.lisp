a62dbf4900caa80d42617560186b89e95e21c636
shka <sirherrbatka@gmail.com>
2017-11-18 13:19:19 +0100

    Added range classes.

--- src/api/fundamental-classes.lisp
+++ src/api/fundamental-classes.lisp
@@ -11,3 +11,20 @@
 
 (defclass fundamental-range ()
   ())
+
+
+(defclass fundamental-forward-range (fundamental-range)
+  ())
+
+
+(defclass fundamental-backward-range (fundamental-range)
+  ())
+
+
+(defclass fundamental-bidirectional-range (fundamental-forward-range
+                                           fundamental-backward-range)
+  ())
+
+
+(defclass fundamental-random-access-range (fundamental-bidirectional-range)
+  ())
