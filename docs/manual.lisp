793253f0fef5f31743880d0680967db378852ff2
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-11-24 21:22:31 +0100

    Corrected docs.

--- docs/manual.lisp
+++ docs/manual.lisp
@@ -8,10 +8,10 @@
   (chunk *cl-data-structures*
     (level [section] @label{cl-ds intro}
       @title{Overview}
-      [:text Cl-data-structures is a portable collection of data structures for ,(lisp). The design goals of this library are following:]
+      [:text Cl-data-structures is a portable collection of data structures for ,(lisp). The design goals of this library are the following:]
 
       (level [list]
-        @item{Uniform -- Data structures that are used for a specific task should have a common interface. The user should just know how to use dictionary, and not some specific implementation of it.}
+        @item{Uniform -- Data structures that are used for a specific task should have a common interface. The user should just know how to use a dictionary, and not some specific implementation of it.}
         @item{Complete -- This package intends to be the definitive ,(lisp) data structures collection, containing both functional and mutable structures, for every use case possible.}
         @item{Universal -- There should be no limitations on when this library is useful.}
         @item{Stable -- The API should be backward compatible. Breaking existing software is not acceptable.})
@@ -22,23 +22,23 @@
 
       (level [section]
         @title{Conventions}
-        [:text Data structure types are not hidden under generic interface name (like "std::unordered_map") but instead are directly exposed to the user. Users are encouraged to read the implementation details section of this manual to decide what data structure implementation works best for the specific use case. Destructive (in the sense of capable of mutating data passed as an argument) functions follow scheme style of adding '!' as a suffix (so we have GF ADD! that is the destructive version of ADD). There are exceptions to this rule, namely SETF functions. According to above, there should be a generic function called INSERT!, but alas, that's not the case. Instead, there is the (SETF AT) API function that does the thing one would expect from INSERT!. In addition to this difference, SETF functions are expected to return value of modified place, and not the container itself. Therefore, that's what (SETF AT) does to maintain style cohesive with ,(lisp).])
+        [:text Data structure types are not hidden under generic interface names (like "std::unordered_map") but are instead directly exposed to the user. Users are encouraged to read the implementation details section of this manual to decide what data structure implementation works best for the specific use case. Destructive (in the sense of capable of mutating data passed as an argument) functions follow the Scheme style of adding '!' as a suffix (so we have GF ADD! that is the destructive version of ADD). There are exceptions to this rule, namely SETF functions. According to the above, there should be a generic function called INSERT!, but alas, that's not the case. Instead, there is the (SETF AT) API function that does the thing one would expect from INSERT!. In addition to this difference, SETF functions are expected to return the value of the modified place, and not the container itself. Therefore, that's what (SETF AT) does to maintain a style cohesive with ,(lisp).])
 
       (level [section]
         @title{Key concepts}
 
-        [:text Inspection of the CL-DATA-STRUCTURE source code may reveal a few interesting patterns. Listed below in their own sections.]
+        [:text Inspection of the CL-DATA-STRUCTURE source code may reveal a few interesting patterns. These patterns are listed below in their own sections.]
 
         (level [section]
           @title{Signaling errors}
           [:text The Cl-data-structures approach to signaling errors can be summarized with two points:]
 
           @begin{list}
-          @item{Signal error, only if without a doubt, error has occurred.}
+          @item{Signal an error, only if without a doubt, error has occurred.}
           @item{Signal only well structured and documented errors.}
           @end{list})
 
-        [:text To fulfill those requirements, library defines it's own hierarchy of conditions, with each error signaled only in a very specific scenario. For instance, there is the INITIALIZATION-OUT-OF-BOUNDS error, that will be signaled only if the user attempts to initialize the class with a value that exceeds accepted bounds, as described in a relevant reference. Such error also usually points to documentation that describes why this error was signaled and provides information on what argument triggered signaling error, and what are an accepted bounds. This is done such way primarily to make both learning and debugging as easy as possible. In addition, it also makes automatic handling of errors actually possible. The user of this library is encouraged to take a look at the error hierarchies, as laid out in this manual API reference section.]
+        [:text To fulfill those requirements, the library defines it's own hierarchy of conditions, with each error signaled only in a very specific scenario. For instance, there is the INITIALIZATION-OUT-OF-BOUNDS error, that will be signaled only if the user attempts to initialize the class with a value that exceeds accepted bounds, as described in a relevant reference. Such error also usually points to documentation that describes why this error was signaled and provides information on what argument triggered signaling error, and what are an accepted bounds. This is done such way primarily to make both learning and debugging as easy as possible. In addition, it also makes automatic handling of errors actually possible. The user of this library is encouraged to take a look at the error hierarchies, as laid out in this manual API reference section.]
         [:text It is also important to point out, that CL-DATA-STRUCTURES attempts to explicitly document every possible error that can be raised by every function. If an unexpected error occurs, it may and should be considered bug of manual itself, and treated as such (namely: ,[:emph reported and fixed]).]
 
         (level [section]
