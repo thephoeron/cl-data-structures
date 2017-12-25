347271fdde0c3bab35c9046e0f406f8af0bc65b5
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-12-26 00:02:02 +0100

    Can't call traverse in accumulate algorithm.

--- src/stat/variance.lisp
+++ src/stat/variance.lisp
@@ -8,41 +8,19 @@
 
 (defgeneric variance (range &key key biased)
   (:generic-function-class entropy-function)
-  (:method (range &key (key #'identity) (biased t))
+  (:method (range &key key (biased t))
     (cl-ds.alg:apply-aggregation-function range
                                           #'variance
                                           :key key
-                                          :range range
                                           :biased biased)))
 
 
-(defstruct variance-state range (count 0 :type fixnum) sum biased key)
-
-
 (defmethod cl-ds.alg:make-state ((function variance-function)
-                                 &rest all &key biased range key
+                                 &rest all &key biased
                                  &allow-other-keys)
   (declare (ignore all))
-  (make-variance-state :range range :biased biased :key key))
+  (list* (vect) biased))
 
 
 (defmethod cl-ds.alg:state-result ((function variance-function) state)
-  (with-accessors ((range variance-state-range)
-                   (count variance-state-count)
-                   (sum variance-state-sum)
-                   (key variance-state-key)
-                   (biased variance-state-biased)) state
-    (let ((total 0)
-          (avg (/ sum count)))
-      (cl-ds:traverse (lambda (x) (incf total (expt (- (funcall key x) avg) 2)))
-                      range)
-      (/ total (- count (if biased 0 1))))))
-
-
-(defmethod cl-ds.alg:aggregate ((function variance-function) state element)
-  (with-accessors ((range variance-state-range)
-                   (count variance-state-count)
-                   (sum variance-state-sum)
-                   (biased variance-state-biased)) state
-    (incf count)
-    (incf sum element)))
+  (alexandria:variance (car state) :biased (cdr state)))
