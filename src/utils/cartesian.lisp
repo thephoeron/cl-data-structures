20dae88038932b7bb52b0b8af333b88d346c7e10
marek <mkochanowicz@ownedoutcomes.com>
2018-01-03 17:22:51 +0100

    Corrected cartesian.

--- src/utils/cartesian.lisp
+++ src/utils/cartesian.lisp
@@ -2,20 +2,20 @@
 
 
 (defun cartesian (sequence-of-sequences result-callback)
-  (let* ((sequence-of-sequences (remove-if #'emptyp sequence-of-sequences))
-         (l (length sequence-of-sequences))
-         (lengths (map '(vector fixnum) #'length sequence-of-sequences))
-         (indexes (make-array l :element-type 'fixnum)))
-    (iterate
-      (for i = (iterate
-                 (for i from 0 below l)
-                 (finding i such-that (not (eql (1+ (aref indexes i))
-                                                (aref lengths i))))))
-      (for p-i previous i initially 0)
-      (apply result-callback
-             (map 'list #'elt sequence-of-sequences indexes))
-      (until (null i))
-      (when (not (eql i p-i))
-        (iterate (for j from 0 below i)
-          (setf (aref indexes j) 0)))
-      (incf (aref indexes i)))))
+  (unless (some #'emptyp sequence-of-sequences)
+    (let* ((l (length sequence-of-sequences))
+           (lengths (map '(vector fixnum) #'length sequence-of-sequences))
+           (indexes (make-array l :element-type 'fixnum)))
+      (iterate
+        (for i = (iterate
+                   (for i from 0 below l)
+                   (finding i such-that (not (eql (1+ (aref indexes i))
+                                                  (aref lengths i))))))
+        (for p-i previous i initially 0)
+        (apply result-callback
+               (map 'list #'elt sequence-of-sequences indexes))
+        (until (null i))
+        (when (not (eql i p-i))
+          (iterate (for j from 0 below i)
+            (setf (aref indexes j) 0)))
+        (incf (aref indexes i))))))
