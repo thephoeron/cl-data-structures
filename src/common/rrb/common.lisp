b5cbef7a8072a54555866eb5a0aee57b17e7f108
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-01-13 20:49:28 +0100

    Added bunch of tests for tail insertion.

--- src/common/rrb/common.lisp
+++ src/common/rrb/common.lisp
@@ -25,8 +25,20 @@
    :type node-content))
 
 
+(defmethod print-object ((obj rrb-node) stream)
+  (format stream "<")
+  (iterate
+    (for elt in-vector (rrb-node-content obj))
+    (for p-elt previous elt)
+    (until (null elt))
+    (unless (null p-elt)
+      (format stream ", "))
+    (format stream "~a" elt))
+  (format stream ">"))
+
+
 (-> rrb-node-deep-copy (rrb-node list) rrb-node)
-(declaim (inline rrb-node-deep-copy))
+(declaim (notinline rrb-node-deep-copy))
 (defun rrb-node-deep-copy (node ownership-tag)
   (make-rrb-node :ownership-tag ownership-tag
                  :content (copy-array (rrb-node-content node))))
@@ -38,7 +50,8 @@
 
 
 (defun rrb-node-push-into-copy (node position element ownership-tag)
-  (let ((result-content (make-array +maximum-children-count+))
+  (let ((result-content (make-array +maximum-children-count+
+                                    :initial-element nil))
         (source-content (rrb-node-content node)))
     (setf (aref result-content position) element)
     (iterate
@@ -63,8 +76,7 @@
 (defclass rrb-container (fundamental-ownership-tagged-object)
   ((%root :accessor access-root
           :initarg :root
-          :initform nil
-          :type (or null rrb-node)
+          :type rrb-node
           :documentation "root of the tree")
    (%shift :initarg :shift
            :accessor access-shift
@@ -78,7 +90,7 @@
                :type node-size
                :accessor access-tail-size)
    (%tail :initform nil
-          :type (or null rrb-node)
+          :type (or null simple-array)
           :initarg :tail
           :accessor access-tail)))
 
@@ -109,56 +121,85 @@
         (setf (content %tail-size) element)
         t))))
 
+
+(declaim (notinline insert-tail))
 (-> insert-tail (rrb-container
                  t
-                 (-> ((or null rrb-node) node-size rrb-node t) rrb-node)
-                 rrb-node) rrb-node)
-(defun insert-tail (rrb-container ownership-tag insert-function tail)
-  (declare (optimize (speed 3)))
-  (bind (((:slots %size %shift %root) rrb-container)
-         (root-overflow (> (the fixnum (ash (the fixnum %size) (- +bit-count+)))
-                           (the fixnum (ash 1 (the shift %shift))))))
-    (if root-overflow
-        (let ((new-node (iterate
-                           (repeat (the non-negative-fixnum %shift))
-                           (for node
-                                initially tail
-                                then (let ((next (make-rrb-node :ownership-tag ownership-tag)))
-                                       (setf (~> node rrb-node-content (aref 0)) node)
-                                       next))
-                           (finally (return node)))))
-          (if (null %root)
-              (values new-node t)
-              (bind ((root (make-rrb-node :ownership-tag ownership-tag))
-                     ((:vectors content) (rrb-node-content root)))
-                (setf (content 0) %root
-                      (content 1) new-node)
-                (values root t))))
-        (let ((path (make-array `(,+maximum-children-count+ 2))))
-          (declare (dynamic-extent path))
-          (iterate
-            (with size = (1- (the non-negative-fixnum %size)))
-            (for i from %shift downto 0)
-            (for position from 0 by 5)
-            (for index = (ldb (byte +bit-count+ position) size))
-            (for node
-                 initially %root
-                 then (and node (~> node rrb-node-content (aref index))))
-            (setf (aref path i 0) node
-                  (aref path i 1) index))
-          (iterate
-            (for i from 0 to %shift)
-            (for existing-node = (aref path i 0))
-            (for index = (the node-size (aref path i 1)))
-            (for next-node
-                 initially (funcall insert-function
-                                    existing-node
-                                    index
-                                    tail
-                                    ownership-tag)
-                 then (funcall insert-function
-                               existing-node
-                               index
-                               next-node
-                               ownership-tag))
-            (finally (return (values next-node nil))))))))
+                 function
+                 node-content)
+    rrb-node)
+(defun insert-tail (rrb-container ownership-tag continue tail)
+  (declare (optimize (debug 3)))
+  (block nil
+    (bind (((:slots %size %shift %root) rrb-container)
+           (root-overflow (>= (the fixnum (ash (the fixnum %size) (- +bit-count+)))
+                              (ash 1 (* +bit-count+ (the shift %shift))))))
+      (if root-overflow
+          (let ((new-node (iterate
+                            (repeat %shift)
+                            (for node
+                                 initially (make-rrb-node :content tail
+                                                          :ownership-tag ownership-tag)
+                                 then (let ((next (make-rrb-node
+                                                   :ownership-tag ownership-tag)))
+                                        (setf (~> next rrb-node-content (aref 0))
+                                              node)
+                                        next))
+                            (finally (return node)))))
+            (bind ((root (make-rrb-node :ownership-tag ownership-tag))
+                   ((:vectors content) (rrb-node-content root)))
+              (setf (content 0) %root
+                    (content 1) new-node)
+              (return (values root t))))
+          (let ((path (make-array +maximum-children-count+
+                                  :initial-element nil))
+                (indexes (make-array +maximum-children-count+
+                                     :element-type `(integer 0 ,+maximum-children-count+))))
+            (declare (dynamic-extent path)
+                     (dynamic-extent indexes))
+            (iterate
+              (with size = (the non-negative-fixnum %size))
+              (for i from 0 below %shift)
+              (for position from (* +bit-count+ %shift) downto 0 by +bit-count+)
+              (for index = (ldb (byte +bit-count+ position) size))
+              (for node
+                   initially %root
+                   then (and node (~> node rrb-node-content (aref index))))
+              (setf (aref path i) node
+                    (aref indexes i) index))
+            (return
+              (values (funcall continue
+                               path
+                               indexes
+                               %shift
+                               ownership-tag
+                               tail)
+                      nil)))))))
+
+
+(-> copy-on-write (t t t t t) t)
+(defun copy-on-write (path indexes shift ownership-tag tail)
+  (declare (optimize (debug 3)))
+  (iterate
+    (for i from 0 below shift)
+    (for position = (aref indexes i))
+    (for old-node = (aref path i))
+    (for node
+         initially (make-rrb-node :content tail
+                                  :ownership-tag ownership-tag)
+         then (if (null old-node)
+                  (let ((n (make-rrb-node :ownership-tag ownership-tag)))
+                    (setf (~> n rrb-node-content (aref position)) node)
+                    n)
+                  (rrb-node-push-into-copy old-node
+                                           position
+                                           node
+                                           ownership-tag)))
+    (finally (return node))))
+
+
+(defun transactional-on-write (path indexes shift ownership-tag))
+
+
+(defun destructive-write (path indexes shift ownership-tag)
+  (declare (ignore ownership-tag)))
--- src/common/rrb/tests.lisp
+++ src/common/rrb/tests.lisp
@@ -1,10 +1,85 @@
 (in-package :cl-user)
 (defpackage rrb-test-suite
-  (:use :prove))
+  (:use :prove :cl))
 (in-package :rrb-test-suite)
 (cl-ds.utils:import-all-package-symbols :cl-data-structures.common.rrb :rrb-test-suite)
 
-(prove:plan 2)
-(is (tail-offset 32) 0)
-(is (tail-offset 500) 480)
-(prove:finalize)
+(progn
+  (prove:plan 2)
+  (is (tail-offset 32) 0)
+  (is (tail-offset 500) 480)
+  (prove:finalize))
+
+(progn
+  (let* ((tail (make-array +maximum-children-count+
+                           :initial-element nil))
+         (tag (make-ownership-tag))
+         (container (make-instance 'rrb-container
+                                   :tail tail
+                                   :size 0)))
+    (map-into tail #'identity
+              (iota +maximum-children-count+))
+    (let ((new-root (insert-tail container
+                         tag
+                         #'copy-on-write
+                         tail)))
+      (is (access-shift container) 0)
+      (is (rrb-node-content new-root) tail :test #'eq)
+      (is (rrb-node-ownership-tag new-root) tag)
+      (let ((another-tail (make-array +maximum-children-count+
+                                      :initial-element nil)))
+        (map-into another-tail #'identity
+                  (iota +maximum-children-count+
+                        :start +maximum-children-count+))
+        (setf (access-root container) new-root)
+        (setf (access-size container) 32)
+        (setf (access-tail-size container) 0)
+        (let ((another-root (insert-tail container
+                                         tag
+                                         #'copy-on-write
+                                         another-tail)))
+          (print another-root)
+          (is (rrb-node-ownership-tag another-root) tag)
+          (is-type (~> another-root rrb-node-content (aref 1))
+                   'rrb-node)
+          (is-type (~> another-root rrb-node-content (aref 0))
+                   'rrb-node)
+          (is (~> another-root rrb-node-content (aref 1) rrb-node-content)
+              another-tail
+              :test #'eq)
+          (ok (not (eq another-root new-root)))
+          (setf (access-root container) another-root))))
+    (setf (access-shift container) 1)
+    (setf (access-size container) (* +maximum-children-count+ +maximum-children-count+))
+    (iterate
+      (for i from 2 below 32)
+      (iterate
+        (for c from 0 below 32)
+        (setf (~> container access-root rrb-node-content (aref i))
+              (make-rrb-node :content
+                              (map 'vector #'identity
+                                   (iota +maximum-children-count+
+                                         :start (* i +maximum-children-count+)))
+                             :ownership-tag tag))))
+    (let* ((another-tail (map 'vector #'identity
+                             (iota +maximum-children-count+
+                                   :start 1024)))
+           (another-root (insert-tail container
+                                      tag
+                                      #'copy-on-write
+                                      another-tail))
+           (result (with-collector (result)
+                     (labels ((impl (node)
+                                (if (rrb-node-p node)
+                                    (iterate
+                                      (for elt in-vector (rrb-node-content node))
+                                      (until (null elt))
+                                      (impl elt))
+                                    (result node))))
+                       (impl another-root)))))
+      (is result (iota (+ 1024 32))
+          :test #'equal)
+      (is (iterate
+            (for elt in-vector (rrb-node-content another-root))
+            (counting elt))
+          2))))
