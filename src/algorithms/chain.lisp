8abfbe06bd65ab556138cd11de2c7c760550063a
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-02-01 17:04:11 +0100

    Added function for creating chained ranges.

--- src/algorithms/chain.lisp
+++ src/algorithms/chain.lisp
@@ -10,4 +10,26 @@
                       :reader read-original-content)))
 
 
-(defgeneric chain (first-range &rest other-ranges))
+(defclass forward-chain-of-ranges (abstract-chain-of-ranges)
+  ())
+
+
+(defclass bidirectional-chain-of-ranges (abstract-chain-of-ranges)
+  ())
+
+
+(defclass random-access-chain-of-ranges (abstract-chain-of-ranges)
+  ())
+
+
+(defun chain (&rest ranges)
+  (map nil
+       (lambda (x) (check-type x cl-ds:fundamental-forward-range))
+       ranges)
+  (let ((fundamental-type (common-fundamental-range-class ranges)))
+    (assert fundamental-type)
+    (make (eswitch (fundamental-type)
+            ('fundamental-forward-range 'forward-chain-of-ranges)
+            ('fundamental-bidirectional-range 'bidirectional-chain-of-ranges)
+            ('fundamental-random-access-range 'random-access-chain-of-ranges))
+          :original-content ranges)))
