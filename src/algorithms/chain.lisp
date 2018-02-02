1842cdaff6ddee6cc8e9314ab6c57b405e1c6a01
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-02-02 11:42:10 +0100

    Added stubs for chain range function.

--- src/algorithms/chain.lisp
+++ src/algorithms/chain.lisp
@@ -3,7 +3,6 @@
 
 (defclass forward-chain-of-ranges (cl-ds:fundamental-forward-range)
   ((%content :initarg :content
-             :initform (make 'flexichain:standard-flexichain)
              :reader read-content)
    (%original-content :initarg :original-content
                       :type list
@@ -20,6 +19,24 @@
   ())
 
 
+(defun init-chain-of-range (obj)
+  (bind (((:slots %content %original-content) obj))
+    (setf %content (make 'flexichain:standard-flexichain))
+    (map nil
+         (lambda (x) (~>> x
+                     cl-ds:clone
+                     (flexichain:push-end %content)))
+         %original-content)))
+
+
+(defmethod initialize-instance :after ((obj forward-chain-of-ranges) &key &allow-other-keys)
+  (init-chain-of-range obj))
+
+
+(defmethod reinitialize-instance ((obj forward-chain-of-ranges) &key &allow-other-keys)
+  (init-chain-of-range obj))
+
+
 (defun chain (&rest ranges)
   (map nil
        (lambda (x) (check-type x cl-ds:fundamental-forward-range))
@@ -31,3 +48,41 @@
             ('fundamental-bidirectional-range 'bidirectional-chain-of-ranges)
             ('fundamental-random-access-range 'random-access-chain-of-ranges))
           :original-content ranges)))
+
+
+(defmethod cl-ds:reset! ((range forward-chain-of-ranges))
+  (reinitialize-instance range)
+  range)
+
+
+(defmethod cl-ds:at ((range random-access-chain-of-ranges) location)
+  (cl-ds.utils:todo))
+
+
+(defmethod cl-ds:size ((range random-access-chain-of-ranges))
+  (cl-ds.utils:todo))
+
+
+(defmethod cl-ds:consume-front ((range forward-chain-of-ranges))
+  (cl-ds.utils:todo))
+
+
+(defmethod cl-ds:peek-front ((range forward-chain-of-ranges))
+  (cl-ds.utils:todo))
+
+
+(defmethod cl-ds:consume-back ((range bidirectional-chain-of-ranges))
+  (cl-ds.utils:todo))
+
+
+(defmethod cl-ds:empty-clone ((range forward-chain-of-ranges))
+  (make (type-of range)
+        :original-content (read-original-content range)))
+
+
+(defmethod cl-ds:traverse (function (range forward-chain-of-ranges))
+  (bind (((:slots %content) range))
+    (iterate
+      (for i from 0 below (flexichain:nb-elements %content))
+      (cl-ds:traverse function (flexichain:element* %content i)))
+    range))
