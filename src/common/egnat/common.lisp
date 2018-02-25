b69c86ced470b0151f03be2062caf0670075e20f
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-02-25 21:41:51 +0100

    Removed bunch of bugs.

--- src/common/egnat/common.lisp
+++ src/common/egnat/common.lisp
@@ -49,16 +49,17 @@
   (bind (((:slots %children) node))
     (unless (null %children)
       (map-into %children #'lparallel:force %children)
-      (map nil #'force-tree %children))))
+      (map nil #'force-tree %children)))
+  node)
 
 
-(defun make-future-egnat-nodes (data number-of-nodes number-of-content metric-fn metric-type)
-  (if (<= (length data) number-of-content)
+(defun make-future-egnat-nodes (data number-of-nodes content-count metric-fn metric-type)
+  (if (<= (length data) content-count)
       (make 'egnat-node :content data)
-      (bind ((this-content (take number-of-content data))
-             (data (drop (length this-content) data))
+      (bind ((this-content (take content-count data))
+             (this-data (drop (length this-content) data))
              (seeds-indexes (iterate
-                              (with generator = (cl-ds.utils:lazy-shuffle 0 (length data)))
+                              (with generator = (cl-ds.utils:lazy-shuffle 0 (length this-data)))
                               (with result = (make-array number-of-nodes
                                                          :element-type 'fixnum
                                                          :fill-pointer 0
@@ -66,7 +67,7 @@
                               (for i = (funcall generator))
                               (repeat number-of-nodes)
                               (while i)
-                              (vector-push-extend (aref data i) result)
+                              (vector-push-extend i result)
                               (finally (return result))))
              (reverse-mapping (let ((table (make-hash-table)))
                                 (iterate
@@ -75,14 +76,14 @@
                                   (setf (gethash i table) j))
                                 table))
              (data-partitions (map '(vector fixnum)
-                                   (lambda (data)
+                                   (lambda (value)
                                      (iterate
                                        (for seed-index in-vector seeds-indexes)
-                                       (for seed = (aref data seed-index))
-                                       (for distance = (funcall metric-fn data seed))
+                                       (for seed = (aref this-data seed-index))
+                                       (for distance = (funcall metric-fn value seed))
                                        (minimize distance into min)
                                        (finding seed-index such-that (= distance min))))
-                                   data))
+                                   this-data))
              (contents (let ((result (make-array number-of-nodes
                                                  :adjustable t
                                                  :fill-pointer (length seeds-indexes))))
@@ -90,18 +91,18 @@
                          (iterate
                            (for d in-vector data-partitions)
                            (for i from 0)
-                           (for element = (aref data i))
+                           (for element = (aref this-data i))
                            (for partition = (gethash d reverse-mapping))
                            (vector-push-extend element (aref result partition)))
-                         result))
+                         (remove-if #'emptyp result)))
              (children (map-into
                         (make-array number-of-nodes
                                     :adjustable t
-                                    :fill-pointer (length seeds-indexes))
+                                    :fill-pointer (length contents))
                         (lambda (content)
                           (lparallel:future
                             (make-future-egnat-nodes content number-of-nodes
-                                                     number-of-content metric-fn metric-type)))
+                                                     content-count metric-fn metric-type)))
                         contents))
              (ranges (cl-ds.utils:make-distance-matrix 'list number-of-nodes))
              (close-range (cl-ds.utils:make-distance-matrix metric-type number-of-nodes))
@@ -114,6 +115,7 @@
              (cl-ds.utils:cartesian
               (list a b)
               (lambda (a b)
+                (break)
                 (let ((distance (coerce (funcall metric-fn a b)
                                         metric-type)))
                   (if (null min)
@@ -124,10 +126,26 @@
                       (setf max (max distance max))))))
              (list* min max)))
          contents)
-        (cl-ds.utils:mutate-matrix close-range #'car ranges)
-        (cl-ds.utils:mutate-matrix distant-range #'cdr ranges)
+        (cl-ds.utils:mutate-matrix close-range
+                                   (lambda (x)
+                                     (if (listp x)
+                                         (car x)
+                                         (coerce 0 metric-type)))
+                                   ranges)
+        (cl-ds.utils:mutate-matrix distant-range
+                                   (lambda (x)
+                                     (if (listp x)
+                                         (cdr x)
+                                         (coerce 0 metric-type)))
+                                   ranges)
         (make 'egnat-node
               :content this-content
               :close-range close-range
               :distant-range distant-range
               :children children))))
+
+
+(defun make-egnat-tree (data number-of-nodes content-count metric-fn metric-type)
+  (~> data
+      (make-future-egnat-nodes number-of-nodes content-count metric-fn metric-type)
+      force-tree))
