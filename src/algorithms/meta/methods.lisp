33f2cc79b418b9f66ce688cbc16c2146f3acb577
marek <mkochanowicz@ownedoutcomes.com>
2018-04-24 11:50:22 +0200

    Ordered methods.lisp file

--- src/algorithms/meta/methods.lisp
+++ src/algorithms/meta/methods.lisp
@@ -1,5 +1,8 @@
 (in-package #:cl-data-structures.algorithms.meta)
 
+#|
+Top level aggregator protocol.
+|#
 
 (defmethod extract-result ((aggregator multi-stage-linear-aggregator))
   (bind (((:slots %accumulator) aggregator))
@@ -11,59 +14,18 @@
     (state-result %function %state)))
 
 
-(defmethod end-aggregation ((aggregator multi-stage-linear-aggregator))
-  (end-aggregation-with-stage (~> aggregator access-stages first)
-                              aggregator))
-
-
-(defmethod initialize-stage ((stage aggregation-stage) (arguments list))
-  (bind (((:slots %name %construct-function %state) stage))
-    (apply %construct-function stage arguments)))
-
-
-(defmethod initialize-stage ((stage cl:function) (arguments list))
-  nil)
-
-
 (defmethod extract-result ((stage aggregation-stage))
   (state-result (read-function stage) (read-state stage)))
 
 
-(defmethod begin-aggregation-with-stage ((stage cl:function)
-                                         (aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %accumulator %arguments) aggregator))
-    (setf %accumulator (apply stage %arguments))
-    (push %accumulator %arguments)))
+(defmethod extract-result ((aggregator linear-aggregator))
+  (bind (((:slots %state %function) aggregator))
+    (state-result %function %state)))
 
 
-(defmethod end-aggregation-with-stage ((stage cl:function)
-                                       (aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %stages) aggregator))
-    (pop %stages)))
-
-
-(defmethod begin-aggregation-with-stage ((stage aggregation-stage)
-                                         (aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %accumulator %arguments) aggregator))
-    (initialize-stage stage %arguments)))
-
-
-(defmethod end-aggregation-with-stage ((stage aggregation-stage)
-                                       (aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %stages %arguments %accumulator) aggregator)
-         (result (extract-result stage)))
-    (setf %accumulator result)
-    (pop %stages)
-    (push result %arguments)
-    (when (slot-boundp stage '%name)
-      (push (read-name stage) %arguments))))
-
-
-(defmethod pass-to-aggregation-with-stage ((stage aggregation-stage)
-                                           (aggregator multi-stage-linear-aggregator)
-                                           element)
-  (bind (((:slots %name %key %construct-function %state %function) stage))
-    (aggregate %function %state (funcall %key element))))
+(defmethod extract-result ((aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %accumulator) aggregator))
+    %accumulator))
 
 
 (defmethod extract-result ((aggregator linear-aggregator))
@@ -76,80 +38,9 @@
                                 aggregator))
 
 
-(defmethod initialize-stage ((stage aggregation-stage) (arguments list))
-  (bind (((:slots %name %construct-function %state) stage))
-    (apply %construct-function stage arguments)))
-
-
-(defmethod initialize-stage ((stage cl:function) (arguments list))
-  nil)
-
-
-(defmethod extract-result ((stage aggregation-stage))
-  (state-result (read-function stage) (read-state stage)))
-
-
-(defmethod begin-aggregation-with-stage ((stage cl:function)
-                                         (aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %accumulator %arguments) aggregator))
-    (setf %accumulator (apply stage %arguments))
-    (push %accumulator %arguments)))
-
-
-(defmethod begin-aggregation-with-stage ((stage aggregation-stage)
-                                         (aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %accumulator %arguments) aggregator))
-    (initialize-stage stage %arguments)))
-
-
-(defmethod pass-to-aggregation-with-stage ((stage aggregation-stage)
-                                           (aggregator multi-stage-linear-aggregator)
-                                           element)
-  (bind (((:slots %name %key %construct-function %state %function) stage))
-    (aggregate %function %state (funcall %key element))))
-
-
-(defmethod apply-range-function ((range cl-ds:fundamental-range)
-                                 (function layer-function)
-                                 &rest all &key &allow-other-keys)
-  (let ((clone (cl-ds:clone range)))
-    (apply #'apply-layer clone function all)))
-
-
-(defmethod apply-aggregation-function (range
-                                       (function aggregation-function)
-                                       &rest all &key key &allow-other-keys)
-  (let* ((aggregator (construct-aggregator range key function nil all)))
-    (apply #'apply-aggregation-function-with-aggregator
-           aggregator range function all)))
-
-
-(defmethod apply-aggregation-function-with-aggregator ((aggregator fundamental-aggregator)
-                                                       range
-                                                       (function aggregation-function)
-                                                       &rest all &key &allow-other-keys)
-  (declare (ignore all))
-  (iterate
-    (until (aggregator-finished-p aggregator))
-    (begin-aggregation aggregator)
-    (until (aggregator-finished-p aggregator))
-    (when (cl-ds.alg.meta:expects-content aggregator)
-      (cl-ds:across (lambda (x)
-                      (pass-to-aggregation aggregator
-                                           x))
-                    range))
-    (end-aggregation aggregator))
-  (extract-result aggregator))
-
-
-(defmethod extract-result ((aggregator multi-stage-linear-aggregator))
-  (bind (((:slots %accumulator) aggregator))
-    %accumulator))
-
-
-(defmethod extract-result ((aggregator linear-aggregator))
-  (bind (((:slots %state %function) aggregator))
-    (state-result %function %state)))
+(defmethod end-aggregation ((aggregator multi-stage-linear-aggregator))
+  (end-aggregation-with-stage (~> aggregator access-stages first)
+                              aggregator))
 
 
 (defmethod pass-to-aggregation ((aggregator multi-stage-linear-aggregator)
@@ -240,6 +131,18 @@
                                 aggregator))
 
 
+(defmethod expects-content ((aggregator linear-aggregator))
+  t)
+
+
+(defmethod expects-content ((aggregator multi-stage-linear-aggregator))
+  (expects-content-with-stage (first (access-stages aggregator))
+                              aggregator))
+
+#|
+Stage level aggregator protocol.
+|#
+
 (defmethod initialize-stage ((stage aggregation-stage) (arguments list))
   (bind (((:slots %name %construct-function %state) stage))
     (apply %construct-function stage arguments)))
@@ -249,27 +152,36 @@
   nil)
 
 
-(defmethod extract-result ((stage aggregation-stage))
-  (state-result (read-function stage) (read-state stage)))
+(defmethod begin-aggregation-with-stage ((stage cl:function)
+                                         (aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %accumulator %arguments) aggregator))
+    (setf %accumulator (apply stage %arguments))
+    (push %accumulator %arguments)))
 
 
-(defmethod expects-content ((aggregator linear-aggregator))
-  t)
+(defmethod begin-aggregation-with-stage ((stage aggregation-stage)
+                                         (aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %accumulator %arguments) aggregator))
+    (initialize-stage stage %arguments)))
 
 
-(defmethod expects-content ((aggregator multi-stage-linear-aggregator))
-  (expects-content-with-stage (first (access-stages aggregator))
-                              aggregator))
+(defmethod begin-aggregation-with-stage ((stage cl:function)
+                                         (aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %accumulator %arguments) aggregator))
+    (setf %accumulator (apply stage %arguments))
+    (push %accumulator %arguments)))
 
 
-(defmethod expects-content-with-stage ((stage fundamental-aggregation-stage)
-                                       (aggregator multi-aggregator))
-  t)
+(defmethod begin-aggregation-with-stage ((stage aggregation-stage)
+                                         (aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %accumulator %arguments) aggregator))
+    (initialize-stage stage %arguments)))
 
 
-(defmethod expects-content-with-stage ((stage cl:function)
-                                       (aggregator multi-aggregator))
-  nil)
+(defmethod end-aggregation-with-stage ((stage cl:function)
+                                       (aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %stages) aggregator))
+    (pop %stages)))
 
 
 (defmethod end-aggregation ((aggregator linear-aggregator))
@@ -284,13 +196,38 @@
   (~> aggregator access-stages endp))
 
 
-(defmethod apply-aggregation-function ((stage aggregation-stage)
-                                       (function aggregation-function)
-                                       &rest all &key key &allow-other-keys)
-  (bind (((:slots %function %key %state) stage))
-    (setf %state (apply #'make-state function all)
-          %function function
-          %key key)))
+(defmethod end-aggregation-with-stage ((stage aggregation-stage)
+                                       (aggregator multi-stage-linear-aggregator))
+  (bind (((:slots %stages %arguments %accumulator) aggregator)
+         (result (extract-result stage)))
+    (setf %accumulator result)
+    (pop %stages)
+    (push result %arguments)
+    (when (slot-boundp stage '%name)
+      (push (read-name stage) %arguments))))
+
+
+(defmethod pass-to-aggregation-with-stage ((stage aggregation-stage)
+                                           (aggregator multi-stage-linear-aggregator)
+                                           element)
+  (bind (((:slots %name %key %construct-function %state %function) stage))
+    (aggregate %function %state (funcall %key element))))
+
+
+(defmethod pass-to-aggregation-with-stage ((stage aggregation-stage)
+                                           (aggregator multi-stage-linear-aggregator)
+                                           element)
+  (bind (((:slots %name %key %construct-function %state %function) stage))
+    (aggregate %function %state (funcall %key element))))
+
+
+(defmethod initialize-stage ((stage aggregation-stage) (arguments list))
+  (bind (((:slots %name %construct-function %state) stage))
+    (apply %construct-function stage arguments)))
+
+
+(defmethod initialize-stage ((stage cl:function) (arguments list))
+  nil)
 
 
 (defmethod begin-aggregation-with-stage ((stage reduce-stage)
@@ -314,3 +251,59 @@
          (access-state stage)
          element
          (access-arguments aggregator)))
+
+
+
+(defmethod expects-content-with-stage ((stage fundamental-aggregation-stage)
+                                       (aggregator multi-aggregator))
+  t)
+
+
+(defmethod expects-content-with-stage ((stage cl:function)
+                                       (aggregator multi-aggregator))
+  nil)
+
+#|
+Range function invokaction protocol.
+|#
+
+(defmethod apply-range-function ((range cl-ds:fundamental-range)
+                                 (function layer-function)
+                                 &rest all &key &allow-other-keys)
+  (let ((clone (cl-ds:clone range)))
+    (apply #'apply-layer clone function all)))
+
+
+(defmethod apply-aggregation-function (range
+                                       (function aggregation-function)
+                                       &rest all &key key &allow-other-keys)
+  (let* ((aggregator (construct-aggregator range key function nil all)))
+    (apply #'apply-aggregation-function-with-aggregator
+           aggregator range function all)))
+
+
+(defmethod apply-aggregation-function-with-aggregator ((aggregator fundamental-aggregator)
+                                                       range
+                                                       (function aggregation-function)
+                                                       &rest all &key &allow-other-keys)
+  (declare (ignore all))
+  (iterate
+    (until (aggregator-finished-p aggregator))
+    (begin-aggregation aggregator)
+    (until (aggregator-finished-p aggregator))
+    (when (cl-ds.alg.meta:expects-content aggregator)
+      (cl-ds:across (lambda (x)
+                      (pass-to-aggregation aggregator
+                                           x))
+                    range))
+    (end-aggregation aggregator))
+  (extract-result aggregator))
+
+
+(defmethod apply-aggregation-function ((stage aggregation-stage)
+                                       (function aggregation-function)
+                                       &rest all &key key &allow-other-keys)
+  (bind (((:slots %function %key %state) stage))
+    (setf %state (apply #'make-state function all)
+          %function function
+          %key key)))
