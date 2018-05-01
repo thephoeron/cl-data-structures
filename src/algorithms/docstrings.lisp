c25a6b29c6a0d2761f0a3ad599880fa42c88b070
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-05-01 12:19:10 +0200

    Added documentation for accumulate.

--- src/algorithms/docstrings.lisp
+++ src/algorithms/docstrings.lisp
@@ -4,6 +4,9 @@
 (docs:define-docs
   :formatter docs.ext:rich-aggregating-formatter
 
+  (function accumulate
+    (:description "Like CL:REDUCE but works on all traversable containers."))
+
   (function group-by
     (:description "Groups RANGE into partitions according to the TEST. This does not change content of RANGE, but it will force aggregation to be performed on every group independently."
      :arguments ((range "Range that is supposed to be groupped.")
