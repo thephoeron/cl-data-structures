ccbc073d9f869610eacf18a8f4533c443f980696
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-05-01 13:06:51 +0200

    Added documentation with count.

--- src/algorithms/docstrings.lisp
+++ src/algorithms/docstrings.lisp
@@ -7,6 +7,10 @@
   (function accumulate
     (:description "Like CL:REDUCE but works on all traversable containers."))
 
+  (function count
+    (:description "Counts number of elements. Usefull mostly in conjuction with GROUP-BY."
+     :see-also (group-by)))
+
   (function group-by
     (:description "Groups RANGE into partitions according to the TEST. This does not change content of RANGE, but it will force aggregation to be performed on every group independently."
      :arguments ((range "Range that is supposed to be groupped.")
