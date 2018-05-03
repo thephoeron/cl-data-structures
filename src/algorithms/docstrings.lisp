6aa9fccf61faaba90602fecd76d88c04d7fb01c9
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-05-03 16:09:22 +0200

    Added stubs for documentation.

--- src/algorithms/docstrings.lisp
+++ src/algorithms/docstrings.lisp
@@ -4,15 +4,28 @@
 (docs:define-docs
   :formatter docs.ext:rich-aggregating-formatter
 
-  (type aggregation-function
-    (:description "Base class of all aggregation functions. Aggregation functions work by applying destructive change to the state for each element in range."))
-
   (function accumulate
     (:description "Like CL:REDUCE but works on all traversable objects."))
+
+  (function hash-join
+    (:description "Joins multiple ranges into one using JOIN-FUNCTION."))
+
+  (function on-each
+    (:description "Creates new range by applying FUNCTION to each element of the RANGE.")
+    (:notes "Works almost like cl:map-and-friends, but it is lazy evaluated. FUNCTION is called only when required."))
 
   (function count
     (:description "Counts number of elements. Usefull mostly in conjuction with GROUP-BY."
      :see-also (group-by)))
+
+  (function chain
+    (:description "Joins multiple ranges sequentially into one."))
+
+  (function summary
+    (:description "Summary is a function that allows to perform multiple aggregations in one form."
+     :arguments ((range "Range to aggregate.")
+                 (forms "Lists describing way to invoke function. First element of list is label used to identify value in the result range, second is aggregation function designator, the rest is list of arguments that should be passed to the function, with range being replaced by the keyword :range."))
+     :returns "Range of results. Use cl-ds:at with label to extract result of each individual aggregation form."))
 
   (function without
     (:description "Layer function. Creates range that skips elements that return T when passed to the PREDICATE function through key function."
