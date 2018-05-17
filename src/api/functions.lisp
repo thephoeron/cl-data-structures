45b8309e0e7992e78ef22ffd25f1f24855476b05
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-05-17 17:34:11 +0200

    Removed %key slot from bootstrap-aggregator.

--- src/api/functions.lisp
+++ src/api/functions.lisp
@@ -11,15 +11,15 @@
 (defun field (name &rest arguments &key &allow-other-keys)
   (lret ((table (make-hash-table :test 'eq))
          (result (make 'field :name name)))
-    (setf (slot-value table '%arguments) table)
+    (setf (slot-value result '%arguments) table)
     (iterate
       (for argument on arguments)
       (for label = (first argument))
       (for value = (second argument))
-      (setf (gethash label result) value))))
+      (setf (gethash label table) value))))
 
 
-(defmethod cl-ds:at ((container cl:hash-table) location &rest more)
+(defmethod cl-ds:at ((container field) location &rest more)
   (assert (endp more))
   (gethash location (read-arguments container)))
 
