fc61e6993e2264458675a51270e2ad3022b4ca41
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-05-19 13:16:47 +0200

    Added eval-always for functions used in macros.

--- src/api/field.lisp
+++ src/api/field.lisp
@@ -18,9 +18,10 @@
       (for label = (first argument))
       (for value = (second argument))
       (for (values v found) = (gethash label table))
-      (error 'invalid-argument
-             :argument label
-             :text "Can't set value in field twice!")
+      (when found
+        (error 'invalid-argument
+               :argument label
+               :text "Can't set value in field twice!"))
       (setf (gethash label table) value))))
 
 
@@ -48,60 +49,57 @@
   (map nil (curry #'validate-field function) fields))
 
 
-(defgeneric accepted-field-arguments (function))
-
-
-(defgeneric validation-form-for (parameter-name argument-name
-                                 field-name value-name
-                                 value-found parameter-body))
-
-
-(defmethod validation-form-for ((parameter-name (eql :optional))
-                                argument-name field-name
-                                value-name value-found body)
-  `(econd
-     ((eq ,body t)
-      (unless ,value-found
-        (return-from nil t)))
-     ((eq ,body nil)
-      (unless ,value-found
-        (return-from nil nil)))))
-
-
-(defmethod validation-form-for ((parameter-name (eql :member))
-                                argument-name field-name
-                                value-name value-found body)
-  `(unless (or (not ,value-found) (member ,value-name '(,@body)))
-     (return-from nil nil)))
-
-
-(defmethod validation-form-for ((parameter-name (eql :default))
-                                argument-name field-name
-                                value-name value-found body)
-  `(unless ,value-found
-     (setf (cl-ds:at ,field-name ,argument-name) ,body)))
-
-
-(defmethod validation-form-for ((parameter-name (eql :type))
-                                argument-name field-name
-                                value-name value-found body)
-  `(when ,value-found
-     (typep ,value-name ,body)))
-
-
-(defun validation-form (parameters-list argument-name field-name)
-  (with-gensyms (!value !found)
-    `(bind (((:values ,!value ,!found) (cl-ds:at ,field-name ,argument-name)))
-       (declare (ignorable ,!value ,!found))
-       ,@(mapcar (lambda (x &aux
-                              (parameter-name (car x))
-                              (parameter-body (cdr x)))
-                   (validation-form-for parameter-name argument-name
-                                        field-name !value !found parameter-body))
-                 parameters-list))))
-
-
 (eval-always
+  (defgeneric validation-form-for (parameter-name argument-name
+                                   field-name value-name
+                                   value-found parameter-body))
+
+
+  (defmethod validation-form-for ((parameter-name (eql :optional))
+                                  argument-name field-name
+                                  value-name value-found body)
+    `(econd
+       ((eq ,body t)
+        (unless ,value-found
+          (return-from nil t)))
+       ((eq ,body nil)
+        (unless ,value-found
+          (return-from nil nil)))))
+
+
+  (defmethod validation-form-for ((parameter-name (eql :member))
+                                  argument-name field-name
+                                  value-name value-found body)
+    `(unless (or (not ,value-found) (member ,value-name '(,@body)))
+       (return-from nil nil)))
+
+
+  (defmethod validation-form-for ((parameter-name (eql :default))
+                                  argument-name field-name
+                                  value-name value-found body)
+    `(unless ,value-found
+       (setf (cl-ds:at ,field-name ,argument-name) ,body)))
+
+
+  (defmethod validation-form-for ((parameter-name (eql :type))
+                                  argument-name field-name
+                                  value-name value-found body)
+    `(when ,value-found
+       (typep ,value-name ,body)))
+
+
+  (defun validation-form (parameters-list argument-name field-name)
+    (with-gensyms (!value !found)
+      `(bind (((:values ,!value ,!found) (cl-ds:at ,field-name ,argument-name)))
+         (declare (ignorable ,!value ,!found))
+         ,@(mapcar (lambda (x &aux
+                                (parameter-name (car x))
+                                (parameter-body (cdr x)))
+                     (validation-form-for parameter-name argument-name
+                                          field-name !value !found parameter-body))
+                   parameters-list))))
+
+
   (defun generate-validation-forms (body field-name accepted-field-arguments)
     (let* ((ordering (ordering accepted-field-arguments)))
       `(block nil
