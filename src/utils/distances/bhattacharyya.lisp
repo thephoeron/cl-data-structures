97f010a3caa9f5c003fe40d729e40c4372774b3e
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-05-21 16:32:56 +0200

    corrected bhattacharyya-distance.

--- src/utils/distances/bhattacharyya.lisp
+++ src/utils/distances/bhattacharyya.lisp
@@ -2,11 +2,15 @@
 
 
 (defun bhattacharyya-distance (h1 h2)
-  (let ((mean1 (mean h1))
-        (mean2 (mean h2))
-        (score (iterate
-                 (for a in-vector h1)
-                 (for b in-vector h2)
-                 (sum (* a b)))))
-    (1- (* score
-           (/ 1 (sqrt (* mean1 mean2 (length h1) (length h2))))))))
+  (let* ((mean1 (mean h1))
+         (mean2 (mean h2))
+         (score (iterate
+                  (for a in-vector h1)
+                  (for b in-vector h2)
+                  (sum (* a b)))))
+    (~>> (* mean1 mean2 (length h1) (length h2))
+         sqrt
+         (/ 1)
+         (* score)
+         1-
+         sqrt)))
