285702093a16139bc2994c1cd3c419223bc3c979
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-05-30 10:29:06 +0200

    Added notice about required equal comparitivness.

--- src/math/docstrings.lisp
+++ src/math/docstrings.lisp
@@ -33,7 +33,7 @@
     (:description "Calculates classical statistical values (average, variance, skewness, kurtosis)."))
 
   (function mutual-information
-    (:description "Calculates mutual-information between FIELD and COMPARATIVE-FIELDS."
+    (:description "Calculates mutual-information between FIELD and COMPARATIVE-FIELDS. Elements in each FIELD should be equal comparable."
      :returns "Range, allowing to access value for each field by quering cl-ds:at with label of field."))
 
   (function moments
