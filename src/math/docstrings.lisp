126f232852a3c688ec24aa867993278bc86b04de
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-06-01 11:11:44 +0200

    Updated documentation.

--- src/math/docstrings.lisp
+++ src/math/docstrings.lisp
@@ -17,14 +17,24 @@
   (function hodges-lehman-estimator
     (:description "Calculates Hodges-Lehman estimator for range."
      :arguments ((range "Object to aggregate")
-                 (key "Function used to extract value from element."))
+                 (key "Function used to extract value from element.")
+                 (parallel "Will use lparallel for calculations if T."))
      :notes ("This estimator has high breakdown point."
              "Time complexity greater then cubic. Memory complexity quadratic."
              "Use bootstrapping for more scalable solution.")
-     :see-also (bootstrap)))
+     :see-also (bootstrap)
+     :thread-safety "Will ues lparallel if PARALLEL is T"))
 
   (function bootstrap
-    (:description "Changes aggregation into bootstrapping schema using percentail method. Reduces time needed to calculate functions with expensive complexity."))
+    (:description "Changes aggregation into bootstrapping schema using percentail method. Reduces time needed to calculate functions with expensive complexity."
+     :arguments ((range "Data for aggregation.")
+                 (sample-size "Size of the single sample.")
+                 (samples-count "Total number of samples drawn.")
+                 (confidence "Confidence value. High confidence results in wide estimate bounds.")
+                 (key "Key passed to SORT function.")
+                 (compare "Function passed to SORT function.")
+                 (parallel "Will use lparallel to evaluate samples if T. Defaults to T."))
+     :thread-safety "Will use lparallel to samples if PARALLEL is T."))
 
   (function simple-linear-regression
     (:description "Matches linear function to RANGE using least squares method."))
