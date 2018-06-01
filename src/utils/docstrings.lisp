112fcc3d2f28619304bc16d525271b5a9fec126b
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-06-01 15:12:10 +0200

    Added documentation for if-else function.

--- src/utils/docstrings.lisp
+++ src/utils/docstrings.lisp
@@ -10,5 +10,9 @@
   (function mref
     (:description "Matrix reference. Accessor for values in matrices."))
 
+  (function if-else
+    (:description "Construct function out of PREDICATE function, TRUE function and FALSE function. Checks if PREDICATE returns true, if yes: pass arguments to the TRUE function, if no: pass arguments to the FALSE function."
+     :returns "Function."))
+
   (function lower-bound
     (:description "Find position in the VECTOR of the first element not larger then ELEMENT.")))
