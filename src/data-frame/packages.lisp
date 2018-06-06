5575d9f4e893adc40405f51d971607639d2c7327
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-06-06 07:05:29 +0200

    Exporting data frame symbols from package.

--- src/data-frame/packages.lisp
+++ src/data-frame/packages.lisp
@@ -5,4 +5,13 @@
   (:use #:common-lisp #:serapeum #:cl-ds.utils
         #:alexandria #:iterate #:metabang-bind)
   (:shadowing-import-from #:iterate #:collecting #:summing #:in)
-  (:nicknames #:cl-ds.df))
+  (:nicknames #:cl-ds.df)
+  (:export
+   #:*active-data*
+   #:*active-frame*
+   #:column
+   #:column-at
+   #:combine
+   #:data
+   #:row
+   #:row-at))
