ff8e541aef3aaa44a892ef282f36fdaa545b6c27
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-06-07 08:31:01 +0200

    Labels on the outside.

--- src/data-frame/methods.lisp
+++ src/data-frame/methods.lisp
@@ -9,26 +9,28 @@
                          #2# #1#))))
 
 
-(defmethod cl-ds:at ((object data-frame) location &rest more)
-  (ensure-dimensionality object more)
-  (labels ((impl (data location)
-             (if (endp location)
-                 data
-                 (impl (cl-ds:at data
-                                 (first location))
-                       (rest location)))))
+(labels ((impl (data location)
+           (if (endp location)
+               data
+               (impl (cl-ds:at data
+                               (first location))
+                     (rest location)))))
+  (defmethod cl-ds:at ((object data-frame) location &rest more)
+    (ensure-dimensionality object more)
     (impl (access-data object)
           (cons location more))))
 
 
-(defmethod (setf cl-ds:at) (new-value (object data-frame) location &rest more)
-  (ensure-dimensionality object more)
-  (labels ((impl (data location)
-             (if (endp (rest location))
-                 (setf (cl-ds:at data (first location))
-                       new-value)
-                 (impl (cl-ds:at data
-                                 (first location))
-                       (rest location)))))
-    (impl (access-data object)
+(labels ((impl (new-value data location)
+           (if (endp (rest location))
+               (setf (cl-ds:at data (first location))
+                     new-value)
+               (impl new-value
+                     (cl-ds:at data
+                               (first location))
+                     (rest location)))))
+  (defmethod (setf cl-ds:at) (new-value (object data-frame) location &rest more)
+    (ensure-dimensionality object more)
+    (impl new-value
+          (access-data object)
           (cons location more))))
