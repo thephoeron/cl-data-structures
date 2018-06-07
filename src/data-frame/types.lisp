ac61de647533060dfb4bd2a011d78448a7347e2d
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-06-07 17:34:53 +0200

    Added fundamental-frame-dimension.

--- src/data-frame/types.lisp
+++ src/data-frame/types.lisp
@@ -5,35 +5,43 @@
   ())
 
 
-(locally (declare (optimize (safety 3)))
-  (defclass data-frame (fundamental-data-frame)
-    ((%data :initarg :data
-            :type cl-ds.seqs.rrb:transactional-rrb-vector
-            :accessor access-data)
-     (%dimensionality :initarg :dimensionality
-                      :type (integer 2 *)
-                      :accessor access-dimensionality
-                      :reader cl-ds:dimensionality)
-     (%sizes :initarg :sizes
-             :reader read-sizes
-             :type (vector non-negative-fixnum))
-     (%aliases :initform (make-hash-table)
-               :reader read-aliases
-               :type hash-table))))
+(nest
+ (locally (declare (optimize (safety 3))))
+ (defclass data-frame (fundamental-data-frame))
+ ((%data :initarg :data
+         :type cl-ds.seqs.rrb:transactional-rrb-vector
+         :accessor access-data)
+  (%dimensionality :initarg :dimensionality
+                   :type (integer 2 *)
+                   :accessor access-dimensionality
+                   :reader cl-ds:dimensionality)
+  (%sizes :initarg :sizes
+          :reader read-sizes
+          :type (vector non-negative-fixnum))
+  (%aliases :initform (make-hash-table)
+            :reader read-aliases
+            :type hash-table)))
 
 
-(locally (declare (optimize (safety 3)))
-  (defclass data-accessor ()
-    ((%position :initarg :position
-                :accessor access-position
-                :type non-negative-fixnum
-                :initform 0)
-     (%frame :initarg :frame
-             :reader read-frame
-             :type data-frame)
-     (%dimension :initarg :dimension
-                 :type non-negative-fixnum
-                 :reader read-dimension)
-     (%data :initarg :data
-            :reader read-data
-            :type cl-ds.seqs.rrb:transactional-rrb-vector))))
+(nest
+ (locally (declare (optimize (safety 3))))
+ (defclass data-accessor ())
+ ((%position :initarg :position
+             :accessor access-position
+             :type non-negative-fixnum
+             :initform 0)
+  (%frame :initarg :frame
+          :reader read-frame
+          :type data-frame)
+  (%dimension :initarg :dimension
+              :type non-negative-fixnum
+              :reader read-dimension)
+  (%data :initarg :data
+         :reader read-data
+         :type cl-ds.seqs.rrb:transactional-rrb-vector)))
+
+
+(nest
+ (locally (declare (optimize (safety 3))))
+ (defclass fundamental-frame-dimension (cl-ds:fundamental-random-access-range))
+ ())
