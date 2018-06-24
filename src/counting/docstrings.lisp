657cc5a97a115c39d57c60c0203d9a4062208399
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-06-24 19:58:37 +0200

    Added documentation for apriori-set and aposteriori-set.

--- src/counting/docstrings.lisp
+++ src/counting/docstrings.lisp
@@ -5,4 +5,17 @@
   :formatter docs.ext:rich-aggregating-formatter
 
   (function find-association
-    (:description "Find and return APRIORI-SET object containing apriori set and aposteriori set. Returned object can be used to obtain association-frequency, support and to find super-sets.")))
+    (:description "Find and return APRIORI-SET object containing apriori set and aposteriori set. Returned object can be used to obtain association-frequency, support and to find super-sets."
+     :arguments ((index "SET-INDEX instance.")
+                 (apriori "List of content in the apriori set.")
+                 (aposteriori "List of content the aposteriori set."))
+     :exceptional-situations ("Will signal cl-ds:not-in-allowed-set if at least one element in either apriori or aposteriori can't be find in the index."
+                              "Will return empty set if set does not exist in the index.")))
+
+  (function apriori-set
+    (:description "Returns SET-IN-INDEX object containing apriori part of association."
+     :arguments ((set "Set containing association."))))
+
+  (function aposteriori-set
+    (:description "Returns SET-IN-INDEX object containing aposteriori part of association."
+     :arguments ((set "Set containing association.")))))
