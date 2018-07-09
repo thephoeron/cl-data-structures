cc6e924213a8a4ec44859f9d6cc687271496dbd9
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-07-09 20:27:07 +0200

    Added option to pin address in cycle-over-address function.

--- src/utils/higher-order.lisp
+++ src/utils/higher-order.lisp
@@ -19,33 +19,51 @@
       (finally (return result)))))
 
 
-(defun cycle-over-address (dimensions)
+(defun cycle-over-address (dimensions &rest pinned)
   (bind ((address (make-list (length dimensions)
                              :initial-element 0))
-         (cell address)
+         (cell nil)
          (finished nil)
-         (current-dimension dimensions))
-    (setf (first address) -1)
-    (lambda ()
-      (unless finished
-        (iterate
-          (for c on address)
-          (for dim on current-dimension)
-          (when (eq c cell)
-            (if (< (1+ (car c)) (car dim))
-                (progn
+         (current-dimension dimensions)
+         (pinned-positions (batches pinned 2))
+         (result nil))
+    (when (oddp (length pinned))
+      (error "Passed odd number of arguments as dimensions to pin"))
+    (bind ((pinned (map 'vector
+                        (lambda (x)
+                          (lret ((result (nthcdr (first x) address)))
+                            (setf (car result) (second x))))
+                        pinned-positions)))
+      (setf result (copy-list address))
+      (lambda ()
+        (if finished
+            (shiftf result nil)
+            (lret ((r result))
+              (setf result (copy-list address))
+              (iterate
+                (for c on address)
+                (for dim on current-dimension)
+                (for pin = (find c pinned :test #'eq))
+                (when pin
+                  (next-iteration))
+                (when (eq c cell)
+                  (if (< (1+ (car c)) (car dim))
+                      (progn
+                        (setf cell c)
+                        (incf (car c))
+                        (leave address))
+                      (progn
+                        (iterate
+                          (for sub on address)
+                          (when (find sub pinned :test #'eq)
+                            (next-iteration))
+                          (until (eq sub c))
+                          (setf (car sub) 0))
+                        (setf (car cell) 0
+                              cell (rest cell))
+                        (next-iteration))))
+                (setf cell c)
+                (when (< (1+ (car c)) (car dim))
                   (incf (car c))
                   (leave address))
-                (progn
-                  (iterate
-                    (for sub on address)
-                    (until (eq sub c))
-                    (setf (car sub) 0))
-                  (setf (car cell) 0
-                        cell (rest cell))
-                  (next-iteration))))
-          (setf cell c)
-          (when (< (1+ (car c)) (car dim))
-            (incf (car c))
-            (leave address))
-          (finally (setf finished t)))))))
+                (finally (setf finished t)))))))))
