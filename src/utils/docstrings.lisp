afccf0276a2a1d1f3af85d15b876cd5a86bc824c
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-08-11 10:13:04 +0200

    Reformatted docstring.

--- src/utils/docstrings.lisp
+++ src/utils/docstrings.lisp
@@ -5,7 +5,7 @@
   :formatter docs.ext:rich-aggregating-formatter
 
   (type half-matrix
-        (:description "Matrix container suitable for storing symetric data (like distances). Does not store diagonal values."))
+    (:description "Matrix container suitable for storing symetric data (like distances). Does not store diagonal values."))
 
   (function mref
     (:description "Matrix reference. Accessor for values in matrices."))
