bdb78d64ad005206ac03bba3891e51bd3a8bdaf2
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-08-13 07:01:06 +0200

    Corrected without-tests package.

--- src/algorithms/without-test.lisp
+++ src/algorithms/without-test.lisp
@@ -1,6 +1,6 @@
 (in-package #:cl-user)
 (defpackage without-tests
-  (:use #:cl #:prove #:serapeum #:cl-ds #:iterate #:alexandria)
+  (:use #:cl #:prove #:cl-ds #:iterate #:alexandria)
   (:shadowing-import-from #:iterate #:collecting #:summing #:in))
 
 (in-package #:without-tests)
@@ -13,10 +13,11 @@
                     (push x result))
                   (cl-ds.alg:without vector #'evenp))
   (is (sort result #'<) '(1 3 5 7 9 11) :test #'equal)
-  (setf result (~> vector
-                   (cl-ds.alg:without #'evenp)
-                   (cl-ds.alg:group-by :key (lambda (x) (mod x 3)))
-                   (cl-ds.alg:accumulate (flip #'cons) _ :initial-value nil)))
+  (setf result (serapeum:~> vector
+                            (cl-ds.alg:without #'evenp)
+                            (cl-ds.alg:group-by :key (lambda (x) (mod x 3)))
+                            (cl-ds.alg:accumulate (flip #'cons)
+                                                  _ :initial-value nil)))
   (is (cl-ds:at result 0) '(9 3) :test #'equal)
   (is (cl-ds:at result 2) '(11 5) :test #'equal)
   (is (cl-ds:at result 1) '(7 1) :test #'equal))
