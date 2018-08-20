b77e1f108c121cd3985b598a33b92de6d5d4ada1
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-08-20 20:57:19 +0200

    Made cartesian consume work fine.

--- src/algorithms/cartesian.lisp
+++ src/algorithms/cartesian.lisp
@@ -8,36 +8,44 @@
    (%function :initarg :function
               :reader read-function)
    (%pivot-index :initform 0
+                 :initarg :pivot-index
                  :accessor access-pivot-index)
-   (%finished :initform nil
-              :accessor access-finished)
-   (%original-content :initarg :orginal-content
-                      :type vector
-                      :reader reader-original-content)))
+   (%original-pivot-index :initform 0
+                          :initarg :original-pivot-index
+                          :reader read-original-pivot-index)))
+
+
+(defmethod cl-ds:reset! ((range forward-cartesian-range))
+  (map nil #'cl-ds:reset! (read-content range))
+  (setf (access-pivot-index range) (read-original-pivot-index range))
+  range)
 
 
 (defmethod cl-ds:clone ((range forward-cartesian-range))
   (make (type-of range)
         :function (read-function range)
-        :finished (access-finished range)
         :pivot-index (access-pivot-index range)
-        :content (map 'vector #'cl-ds:clone (read-content range))
-        :original-content (map 'vector #'cl-ds:clone (read-content range))))
+        :original-pivot-index (access-pivot-index range)
+        :content (map 'vector #'cl-ds:clone (read-content range))))
 
 
 (defun cartesian-consume-front-cycle (range)
   (bind (((:slots %content %pivot-index) range))
     (unless (nth-value 1 (cl-ds:peek-front (aref %content %pivot-index)))
       (incf %pivot-index))
-    (iterate
-      (for i from 0 below %pivot-index)
-      (cl-ds:reset! (aref %content i)))
     (if (eql %pivot-index (length %content))
-        (progn (setf (access-finished range) t)
+        (progn (setf (access-pivot-index range) (length %content))
                (values nil nil))
-        (progn
-          (cl-ds:consume-front (aref %content %pivot-index))
-          (cl-ds:consume-front range)))))
+        (iterate
+          (for i from 0 below %pivot-index)
+          (cl-ds:reset! (aref %content i))
+          (finally
+           (cl-ds:consume-front (aref %content %pivot-index))
+           (if (nth-value 1 (cl-ds:peek-front (aref %content %pivot-index)))
+               (return (cl-ds:consume-front range))
+               (progn
+                 (setf (access-pivot-index range) (length %content))
+                 (return (values nil nil)))))))))
 
 
 (defun cartesian-peek-front-cycle (range)
@@ -59,21 +67,22 @@
 
 
 (defmethod cl-ds:consume-front ((range forward-cartesian-range))
-  (if (access-finished range)
+  (if (eql (access-pivot-index range) (~> range read-content length))
       (values nil nil)
       (bind (((:slots %content) range)
              (pivot-range (aref %content 0))
              ((:values _ more) (cl-ds:peek-front pivot-range)))
         (if more
-            (prog1 (values (apply (read-function range)
-                                  (map 'list #'cl-ds:peek-front %content))
-                           t)
+            (multiple-value-prog1
+                (values (apply (read-function range)
+                               (map 'list #'cl-ds:peek-front %content))
+                        t)
               (cl-ds:consume-front pivot-range))
             (cartesian-consume-front-cycle range)))))
 
 
 (defmethod cl-ds:peek-front ((range forward-cartesian-range))
-  (if (access-finished range)
+  (if (eql (access-pivot-index range) (~> range read-content length))
       (values nil nil)
       (bind (((:slots %content) range)
              (pivot-range (aref %content 0))
@@ -83,3 +92,15 @@
                            (map 'list #'cl-ds:peek-front %content))
                     t)
             (cartesian-peek-front-cycle range)))))
+
+
+(defun cartesian (function range &rest more-ranges)
+  (declare (dynamic-extent more-ranges))
+  (check-type range fundamental-range)
+  (iterate
+    (for r in more-ranges)
+    (check-type r fundamental-range))
+  (make 'forward-cartesian-range
+        :function function
+        :content (~> (cons range more-ranges)
+                     (coerce 'vector))))
