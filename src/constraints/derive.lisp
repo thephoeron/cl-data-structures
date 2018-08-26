691ea9b5ecb7163cc4a0bc9aa70d08633ea022c5
shka <sirherrbatka@gmail.com>
2018-08-26 10:43:10 +0200

    Added stub for derive macro.

---
+++ src/constraints/derive.lisp
@@ -0,0 +1,18 @@
+(in-package #:cl-data-structures.constraints)
+
+
+(defmacro derive (bindings body)
+  (with-gensyms (!depth !next !this)
+    (let ((all-symbols (mapcar #'first bindings)))
+      `(lambda (&optional (depth 0))
+         (let* (,@all-symbols
+                (,!next (funcall ,body (1+ depth)))
+                (,!depth depth)
+                (,!this nil))
+           (setf ,!this
+                 (lambda ()
+                   (if (some #'reached-end '(,@all-symbols))
+                       (values nil nil)
+                       (funcall ,!next))))
+           ,(initialize-bindings-form bindings !this !depth)
+           ,!this)))))
---
+++ src/constraints/package.lisp
@@ -0,0 +1,10 @@
+(in-package #:cl-user)
+
+
+(defpackage :cl-data-structures.constraints
+  (:use #:common-lisp
+        #:cl-data-structures.aux-package
+        #:cl-data-structures.utils)
+  (:nicknames #:cl-ds.constraints)
+  (:export))
+
