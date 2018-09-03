57f0323a24d1cccbf9eb033e26c02c5d5f4c0489
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-09-03 07:55:49 +0200

    Added consume-front and peek-front.

--- src/constraints/constraints.lisp
+++ src/constraints/constraints.lisp
@@ -34,9 +34,40 @@
           :inputs *all-inputs*)))
 
 
-(defmethod cl-ds:consume-front ((range constrainted-range))
-  )
-
-
 (defmethod cl-ds:peek-front ((range constrainted-range))
-  )
+  (bind ((current (access-current range))
+         ((:labels value (element))
+          (cond ((null element) nil)
+                ((atom element) element)
+                ((listp element)
+                 (iterate
+                   (for elt in element)
+                   (for result = (value elt))
+                   (until result)
+                   (finally (return result))))))
+         (result (value current)))
+    (if (null result)
+        (~> range cl-ds:clone cl-ds:consume-front)
+        (values result t))))
+
+
+(defmethod cl-ds:consume-front ((range constrainted-range))
+  (bind (((:accessor (current access-current)) range)
+         ((:labels value ())
+          (cond ((null current) nil)
+                ((atom current) (shiftf current nil))
+                ((listp current)
+                 (iterate
+                   (for elt = (first current))
+                   (setf current (rest current))
+                   (cond ((listp elt) (setf current (append elt current)))
+                         ((atom elt) (leave elt)))
+                   (until (null current))))))
+         (result (value)))
+    (if (null result)
+        (let ((val (~> range access-monad funcall)))
+          (if (eql val :end)
+              (values nil nil)
+              (progn (setf current val)
+                     (cl-ds:consume-front range))))
+        (values result t))))
