e29d4d13b96d5fee96855d69c71b77b4c5c0b683
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-09-05 14:57:18 +0200

    Hide binding in with-file-ranges.

--- src/file-system/common.lisp
+++ src/file-system/common.lisp
@@ -22,20 +22,24 @@
 
 
 (defmacro with-file-ranges (bindings &body body)
-  (with-gensyms (!tmp)
-    `(let ,(mapcar (lambda (x) (list (first x) nil)) bindings)
-       (unwind-protect
-            (progn
-              ,@(mapcar (lambda (x) `(setf ,(first x)
-                                      (let ((,!tmp ,(second x)))
-                                        (check-type ,!tmp file-range-mixin)
-                                        ,!tmp)))
-                        bindings)
-              ,@body)
-         (progn
-           ,@(mapcar (lambda (x) `(unless (null ,(first x))
-                               (close-inner-stream ,(first x))))
-                     bindings))))))
+  (let ((extra-vars (mapcar (lambda (x) (gensym)) bindings)))
+    (with-gensyms (!tmp)
+      `(let (,@extra-vars ,@(mapcar (lambda (x) (list (first x) nil)) bindings))
+         (unwind-protect
+              (progn
+                ,@(mapcar (lambda (extra x)
+                            `(setf ,(first x)
+                                   (let ((,!tmp ,(second x)))
+                                     (check-type ,!tmp file-range-mixin)
+                                     ,!tmp)
+                                   ,extra ,(first x)))
+                          extra-vars
+                          bindings)
+                ,@body)
+           (progn
+             ,@(mapcar (lambda (x) `(unless (null ,x)
+                                 (close-inner-stream ,x)))
+                       extra-vars)))))))
 
 
 (defmethod initialize-instance :after ((range file-range-mixin)
