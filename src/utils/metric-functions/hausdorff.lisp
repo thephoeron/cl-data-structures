485754c1e4c84ad79e3664e7c7e7169ac603bd41
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-09-12 09:47:05 +0200

    Reformatted code.

--- src/utils/metric-functions/hausdorff.lisp
+++ src/utils/metric-functions/hausdorff.lisp
@@ -4,9 +4,9 @@
 (defun hausdorff-metric (a b fn
                          &key
                            (element-type t)
-                           (distance-matrix (make-array (list (length a)
-                                                              (length b))
-                                                        :element-type element-type)))
+                           (distance-matrix
+                            (make-array (list (length a) (length b))
+                                        :element-type element-type)))
   (declare (type vector a) (type vector b)
            (optimize (speed 3)))
   (ensure-functionf fn)
