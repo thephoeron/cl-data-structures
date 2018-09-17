52f28091cce91de9d51b7f03d3c51c2a07c15db5
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-09-17 08:09:06 +0200

    check for null bucket at the end.

--- src/common/2-3-tree/tests.lisp
+++ src/common/2-3-tree/tests.lisp
@@ -2,7 +2,7 @@
 (defpackage 2-3-tree-tests (:use :cl :prove :cl-data-structures.aux-package))
 (in-package :2-3-tree-tests)
 
-(plan 64)
+(plan 65)
 
 (let ((node cl-ds.meta:null-bucket)
       (i 31))
@@ -29,6 +29,7 @@
       (for i from 0 below 32)
       (for (values new-node old-value) = (cl-ds.common.2-3::delete-back-from-tree node))
       (setf node new-node)
-      (is i old-value))))
+      (is i old-value))
+    (is node cl-ds.meta:null-bucket)))
 
 (finalize)
