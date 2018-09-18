3160af7c1da5effde0bfb49ec86808520f20be57
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-09-18 10:47:35 +0200

    Working on posix find clone.

---
+++ src/file-system/find.lisp
@@ -0,0 +1,69 @@
+(in-package #:cl-data-structures.file-system)
+
+
+(defclass find-range (cl-ds:fundamental-forward-range)
+  ((%stack :initarg :stack
+           :initform nil
+           :type list
+           :accessor access-stack)))
+
+
+(defgeneric find (description)
+  ())
+
+
+;; ((:directory :path "/home/shka/Muzyka")
+;;  (:regex-directory :regex "*"
+;;   :times '(0 :recursive))
+;;  (:regex-file :regex "*.lisp"))
+
+
+(defclass fundamental-file-range-stack-cell ()
+  ((%prev-cell :initarg :prev-cell
+               :initform nil
+               :type fundamental-file-range-stack-cell
+               :accessor access-prev-cell)
+   (%path :initarg :path
+          :type string
+          :reader read-path)))
+
+
+(defclass regex-file-range-stack-cell (fundamental-file-range-stack-cell)
+  ())
+
+
+(defclass directory-file-range-stack-cell (fundamental-file-range-stack-cell)
+  ())
+
+
+(defclass regex-directory-file-range-stack-cell (directory-file-range-stack-cell
+                                                 regex-file-range-stack-cell)
+  ())
+
+
+(defclass file-file-range-stack-cell (fundamental-file-range-stack-cell)
+  ())
+
+
+(defclass regex-file-file-range-stack-cell (fundamental-file-range-stack-cell
+                                            regex-file-range-stack-cell)
+  ())
+
+
+(defgeneric make-stack-cell (name &key &allow-other-keys))
+
+
+(defmethod make-stack-cell ((name (eql :in-directory)) &key path)
+  (make 'directory-file-range-stack-cell
+        :path path))
+
+
+(defmethod make-stack-cell ((name (eql :regex-directory)) &key regex times)
+  (make 'regex-directory-file-range-stack-cell
+        :path regex
+        :times times))
+
+
+(defmethod make-stack-cell ((name (eql :regex-file)) &key regex)
+  (make 'regex-directory-file-range-stack-cell
+        :path regex)
--- src/file-system/package.lisp
+++ src/file-system/package.lisp
@@ -4,6 +4,7 @@
 (defpackage :cl-data-structures.file-system
   (:use #:cl-data-structures.aux-package #:common-lisp)
   (:nicknames #:cl-ds.fs)
+  (:shadow #:find)
   (:export
    #:line-by-line
    #:close-inner-stream
