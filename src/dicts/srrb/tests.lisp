bbe94cffbe2422af5eb96feeab53590d0b187417
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-09-28 09:41:33 +0200

    Added first test for srrb.

--- src/dicts/srrb/tests.lisp
+++ src/dicts/srrb/tests.lisp
@@ -3,3 +3,13 @@
   (:use :cl :prove :cl-data-structures.aux-package)
   (:shadowing-import-from :iterate :collecting :summing :in))
 (in-package :sparse-rrb-vector-tests)
+
+(plan 1)
+
+(let* ((tail (make-array (cl-ds.common.rrb:+maximum-children-count+)))
+       (vector (make-instance 'cl-ds.dicts.srrb::mutable-sparse-rrb-vector
+                              :tail tail)))
+  (cl-ds.dicts.srrb::insert-tail! vector nil)
+  (is (cl-ds.dicts.srrb::access-tree vector) tail))
+
+(finalize)
