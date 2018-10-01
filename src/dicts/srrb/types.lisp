e269e29cb4fa77dddd5c03e163999f6297613603
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-10-01 18:15:04 +0200

    Set tail to nil if tail array was put into the tree.

--- src/dicts/srrb/types.lisp
+++ src/dicts/srrb/types.lisp
@@ -157,6 +157,8 @@
                     (setf (access-tail structure) nil)))))))
     (setf (access-tail-mask structure) 0
           (access-tree-index-bound structure) (access-index-bound structure))
+    (when (eql tail-mask cl-ds.common.rrb:+tail-mask+)
+      (setf (access-tail structure) nil))
     (incf (access-index-bound structure)
           cl-ds.common.rrb:+maximum-children-count+)
     (incf (access-tree-size structure) (logcount tail-mask)))
