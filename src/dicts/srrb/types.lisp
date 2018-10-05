da9bb36130a1f51e40f0d7706de0b098339dc1c2
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-10-05 10:27:13 +0200

    Remove locally declaration.

--- src/dicts/srrb/types.lisp
+++ src/dicts/srrb/types.lisp
@@ -1,33 +1,31 @@
 (in-package #:cl-data-structures.dicts.srrb)
 
 
-(locally (declare (optimize (safety 3)))
-  (defclass fundamental-sparse-rrb-vector
-      (cl-ds.dicts:fundamental-sparse-vector)
-    ((%tree :initarg :tree
-            :initform nil
-            :accessor access-tree)
-     (%tail :initarg :tail
-            :accessor access-tail
-            :initform nil)
-     (%tail-mask :initarg :tail-mask
-                 :initform 0
-                 :accessor access-tail-mask)
-     (%shift :initarg :shift
-             :accessor access-shift
-             :initform 0)
-     (%tree-size :initarg :tree-size
-                 :accessor access-tree-size
-                 :initform 0)
-     (%tree-index-bound :initarg :tree-index-bound
-                        :initform 0
-                        :accessor access-tree-index-bound)
-     (%index-bound :initarg :index-bound
-                   :accessor access-index-bound
-                   :initform cl-ds.common.rrb:+maximum-children-count+)
-     (%element-type :initarg :element-type
-                    :reader read-element-type
-                    :initform t))))
+(defclass fundamental-sparse-rrb-vector (cl-ds.dicts:fundamental-sparse-vector)
+  ((%tree :initarg :tree
+          :initform nil
+          :accessor access-tree)
+   (%tail :initarg :tail
+          :accessor access-tail
+          :initform nil)
+   (%tail-mask :initarg :tail-mask
+               :initform 0
+               :accessor access-tail-mask)
+   (%shift :initarg :shift
+           :accessor access-shift
+           :initform 0)
+   (%tree-size :initarg :tree-size
+               :accessor access-tree-size
+               :initform 0)
+   (%tree-index-bound :initarg :tree-index-bound
+                      :initform 0
+                      :accessor access-tree-index-bound)
+   (%index-bound :initarg :index-bound
+                 :accessor access-index-bound
+                 :initform cl-ds.common.rrb:+maximum-children-count+)
+   (%element-type :initarg :element-type
+                  :reader read-element-type
+                  :initform t)))
 
 
 (defclass mutable-sparse-rrb-vector (cl-ds:mutable
