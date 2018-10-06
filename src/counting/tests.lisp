ef1911b7580bb072b82d09bc345933a1544007ed
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-10-06 15:08:54 +0200

    Corrected package for tests.

--- src/counting/tests.lisp
+++ src/counting/tests.lisp
@@ -1,7 +1,6 @@
 (in-package #:cl-user)
 (defpackage apriori-tests
-  (:use #:cl #:prove #:serapeum #:cl-ds #:iterate #:alexandria :metabang-bind)
-  (:shadowing-import-from #:iterate #:collecting #:summing #:in))
+  (:use :cl :prove :cl-data-structures.aux-package))
 
 (in-package #:apriori-tests)
 
