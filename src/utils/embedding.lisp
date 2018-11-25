32486f21e3bd9279375d1ef0c534c63b545b5aa5
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-11-25 10:41:31 +0100

    Added optimization hints for the embeddings function.

--- src/utils/embedding.lisp
+++ src/utils/embedding.lisp
@@ -9,39 +9,42 @@
            (type boolean parallel))
   (ensure-functionf distance-fn)
   (check-type vector-of-elements vector)
-  (cases ((simple-vector-p vector-of-elements)
-          (array-has-fill-pointer-p vector-of-elements)
-          parallel
-          (:variant
-           (eq embedding-type 'fixnum)
-           (eq embedding-type 'single-float)
-           (eq embedding-type 'double-float)))
-    (bind ((length (length vector-of-elements))
-           (embeddings (make-array length))
-           (m1 (ceiling (log length 2)))
-           (m2 (ceiling (* c (log length))))
-           (element-type (array-element-type vector-of-elements))
-           ((:dflet sample-size (j))
-            (ceiling (* length (expt 2 (- j)))))
-           (sample (make-array (sample-size 1)
-                               :fill-pointer 0
-                               :element-type element-type)))
-      (map-into embeddings (curry #'make-array (* m1 m2)
-                                  :element-type embedding-type
-                                  :fill-pointer 0))
-      (iterate
-        (declare (type fixnum j))
-        (for j from 1 to m1)
-        (iterate
-          (declare (type fixnum i))
-          (for i from 1 to m2)
-          (draw-sample-vector vector-of-elements (sample-size j) sample)
-          (funcall (if parallel #'lparallel:pmap #'map) nil
-                   (lambda (embedding x)
-                     (iterate
-                       (for other-x in-vector sample)
-                       (minimize (funcall distance-fn x other-x) into distance)
-                       (finally (vector-push-extend distance embedding))))
-                   embeddings
-                   vector-of-elements)))
-      embeddings)))
+  (nest
+   (cases ((simple-vector-p vector-of-elements)
+           (array-has-fill-pointer-p vector-of-elements)
+           parallel
+           (:variant
+            (eq embedding-type 'fixnum)
+            (eq embedding-type 'single-float)
+            (eq embedding-type 'double-float))))
+   (let* ((length (length vector-of-elements))
+          (embeddings (make-array length))
+          (m1 (ceiling (log length 2)))
+          (m2 (ceiling (* c (log length))))
+          (element-type (array-element-type vector-of-elements)))
+     (declare (type fixnum length m1 m2)
+              (type simple-vector embeddings)))
+   (bind (((:dflet sample-size (j))
+           (the fixnum (ceiling (the fixnum (* length (expt 2 (- j)))))))
+          (sample (make-array (sample-size 1)
+                              :fill-pointer 0
+                              :element-type element-type)))
+     (map-into embeddings (curry #'make-array (the fixnum (* m1 m2))
+                                 :element-type embedding-type
+                                 :fill-pointer 0))
+     (iterate
+       (declare (type fixnum j))
+       (for j from 1 to m1)
+       (iterate
+         (declare (type fixnum i))
+         (for i from 1 to m2)
+         (draw-sample-vector vector-of-elements (sample-size j) sample)
+         (funcall (if parallel #'lparallel:pmap #'map) nil
+                  (lambda (embedding x)
+                    (iterate
+                      (for other-x in-vector sample)
+                      (minimize (funcall distance-fn x other-x) into distance)
+                      (finally (vector-push-extend distance embedding))))
+                  embeddings
+                  vector-of-elements)))
+     embeddings)))
