d7dd345d5a9bc3364412265d050e4c9b5c693c37
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2018-11-30 11:10:13 +0100

    Added :chunk-sze-hint to the in-parallel function.

--- src/threads/in-parallel.lisp
+++ src/threads/in-parallel.lisp
@@ -5,6 +5,8 @@
                              cl-ds.alg:proxy-range)
   ((%limit :initarg :limit
            :reader read-limit)
+   (%chunk-size-hint :initarg :chunk-size-hint
+                     :reader read-chunk-size-hint)
    (%context-function :initarg :context-function
                       :reader read-context-function)))
 
@@ -27,6 +29,7 @@
 (defmethod cl-ds:clone ((range in-parallel-range))
   (make (type-of range)
         :limit (read-limit range)
+        :chunk-size-hint (read-chunk-size-hint range)
         :context-function (read-context-function range)
         :original-range (cl-ds.alg:read-original-range range)))
 
@@ -39,24 +42,25 @@
           (lret ((vector (make-array 128 :adjustable t :fill-pointer 0)))
             (cl-ds:traverse (rcurry #'vector-push-extend vector) x)))
          ((:flet impl ())
-          (funcall context-function
-                   (lambda ()
-                     (cl-ds:traverse
-                      (lambda (x)
-                        (handler-case (~> (to-vector x)
-                                          (list* t)
-                                          lparallel:future
-                                          push-queue)
-                          (error (e) (push-queue (list* e :error)))))
-                      chunked-range)))
+          (handler-case
+              (funcall context-function
+                       (lambda ()
+                         (cl-ds:traverse
+                          (lambda (x)
+                            (push-queue
+                             (lparallel:future
+                               (list (to-vector x) t))))
+                          chunked-range)))
+            (error (e) (push-queue (list* e :error))))
           (push-queue (list* nil nil)))
-         (thread (bt:make-thread #'impl :name "in-parallel dispatching thread")))
+         (thread (bt:make-thread
+                  #'impl :name "in-parallel dispatching thread")))
     (list* thread queue)))
 
 
 (defun traverse/accross-thread-buffer-range (traverse/accross range function)
   (bind ((og-range (cl-ds.alg::read-original-range range))
-         (chunked-range (cl-ds:chunked og-range)))
+         (chunked-range (cl-ds:chunked og-range (read-chunk-size-hint range))))
     (if (null chunked-range)
         (funcall traverse/accross function og-range)
         (progn
@@ -93,36 +97,41 @@
   (:metaclass closer-mop:funcallable-standard-class))
 
 
-(defgeneric in-parallel (range &key limit context-function)
+(defgeneric in-parallel (range &key limit context-function chunk-size-hint)
   (:generic-function-class in-parallel-function)
-  (:method (range &key (limit 512) (context-function #'funcall))
+  (:method (range &key (limit 512) (context-function #'funcall) chunk-size-hint)
     (cl-ds.alg.meta:apply-range-function range #'in-parallel
                                          :limit limit
+                                         :chunk-size-hint chunk-size-hint
                                          :context-function context-function)))
 
 
 (defmethod cl-ds.alg.meta:apply-layer ((range cl-ds:fundamental-forward-range)
                                        (fn in-parallel-function)
-                                       &rest all &key limit context-function)
+                                       &rest all
+                                       &key limit chunk-size-hint context-function)
   (declare (ignore all))
   (cl-ds.alg:make-proxy range 'forward-in-parallel-range
                         :limit limit
+                        :chunk-size-hint chunk-size-hint
                         :context-function context-function))
 
 
 (defmethod cl-ds.alg.meta:apply-layer ((range cl-ds:fundamental-random-access-range)
                                        (fn in-parallel-function)
-                                       &rest all &key limit context-function)
+                                       &rest all &key limit chunk-size-hint context-function)
   (declare (ignore all))
   (cl-ds.alg:make-proxy range 'random-in-parallel-range
                         :limit limit
+                        :chunk-size-hint chunk-size-hint
                         :context-function context-function))
 
 
 (defmethod cl-ds.alg.meta:apply-layer ((range cl-ds:fundamental-bidirectional-range)
                                        (fn in-parallel-function)
-                                       &rest all &key limit context-function)
+                                       &rest all &key chunk-size-hint limit context-function)
   (declare (ignore all))
   (cl-ds.alg:make-proxy range 'bidirectional-in-parallel-range
                         :limit limit
+                        :chunk-size-hint chunk-size-hint
                         :context-function context-function))
