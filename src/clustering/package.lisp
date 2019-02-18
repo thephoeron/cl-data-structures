b2bdbb168ebcf24f1b5d74f8ff1438f40c9594c6
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-02-18 12:48:40 +0100

    Remove mistakenly placed shadow in defpackage.

--- src/clustering/package.lisp
+++ src/clustering/package.lisp
@@ -3,7 +3,6 @@
 
 (defpackage :cl-data-structures.clustering
   (:use #:cl-data-structures.aux-package #:common-lisp)
-  (:shadow #:variance #:standard-deviation)
   (:nicknames #:cl-ds.cluster)
   (:export
    #:clara
