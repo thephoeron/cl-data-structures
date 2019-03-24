75364fbf5492872c8d73d2eee08a7dffc230e988
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-03-24 19:43:17 +0100

    Added common code for silhouette calculation.

--- src/utils/clustering/package.lisp
+++ src/utils/clustering/package.lisp
@@ -2,8 +2,4 @@
   (:use #:cl #:cl-data-structures.aux-package)
   (:nicknames #:cl-ds.utils.cluster)
   (:export
-   #:clara
-   #:clara-variable-number-of-medoids
-   #:partition-around-medoids
-   #:read-cluster-contents
-   #:read-silhouette))
+   ))
