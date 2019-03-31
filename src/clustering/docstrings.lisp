a39418289b91de21dbd7495012bb79bfe826231d
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-03-31 17:02:25 +0200

    Updated notes on the k-means.

--- src/clustering/docstrings.lisp
+++ src/clustering/docstrings.lisp
@@ -7,11 +7,13 @@
   (function k-means
     (:description "Clusters data set using k-means algorithm. Data points must be represented as one dimensional simple-arrays specialized for single-floats."
      :returns "Clusters object. Clusters itself are represented as vector of vectors and can be read using CLUSTER-CONTENTS function. Addiditionally, silhouette values can be read with SILHOUETTE function."
+     :notes "This algorithm does not calculate silhouette during it's operation and because of that calling silhouette on the result will take extra time."
      :thread-safety "Uses lparallel underneath."))
 
   (function clara
     (:description "Clusters data set using CLARA algorithm. This algorithm attempts to cluster random subset, picking best set of clusters."
-     :notes "Useful for clustering large data sets, as required memory is linear to the size of data set and quadratic to the size of the sample."
+     :notes ("Useful for clustering large data sets, as required memory is linear to the size of data set and quadratic to the size of the sample."
+             "Algorithm initially described in the Clustering Large Applications article.")
      :arguments ((range "Data to cluster.")
                  (number-of-medoids "How many clusters should be created?")
                  (sample-size "Size of sample used to cluster data.")
@@ -21,7 +23,7 @@
                  (key "Function used to extract value for metric-fn.")
                  (select-medoids-attempts-count "How many times PAM should attempt to select medoids before accepting suboptimal medoids?")
                  (attempts "How many times clusters should be splitted and merged before accepting suboptimal cluster sizes.")
-                 (split "Threshold size of clusters. Clusters above this size will attempt to be splitted.")
-                 (merge "Threshold size of clusters. Clusters below this size will attempt to be merged."))
+                 (split "Threshold size of clusters. Function will attempt to split clusters above this size.")
+                 (merge "Threshold size of clusters. Function will attempt to merge clusters below this size."))
      :returns "Clusters object. Clusters itself are represented as vector of vectors and can be read using CLUSTER-CONTENTS function. Addiditionally, silhouette values can be read with SILHOUETTE function."
      :thread-safety "Uses lparallel underneath.")))
