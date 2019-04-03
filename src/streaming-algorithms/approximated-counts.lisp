b8c0f8fb5900b615a3142fef2e1f54becd7e8b97
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-04-03 15:41:14 +0200

    Check types of approximated-count slots.

--- src/streaming-algorithms/approximated-counts.lisp
+++ src/streaming-algorithms/approximated-counts.lisp
@@ -7,36 +7,45 @@
               :reader read-counters)
    (%hashes :initarg :hashes
             :type vector
-            :reader read-hashes)
+            :accessor access-hashes)
    (%count :initarg :count
            :type fixnum
-           :reader read-count)
+           :accessor access-count)
    (%space :initarg :space
            :type fixnum
-           :reader read-space)
+           :accessor access-space)
    (%size :initarg :size
           :initform 0
           :type integer
           :accessor access-size
           :reader cl-ds:size)
    (%hash-fn :initarg :hash-fn
-             :reader read-hash-fn
+             :accessor access-hash-fn
              :type function)))
 
 
+(defmethod initialize-instance :after ((object approximated-counts)
+                                       &rest all)
+  (declare (ignore all))
+  (ensure-functionf (access-hash-fn object))
+  (check-type (access-space object) positive-fixnum)
+  (check-type (access-count object) positive-fixnum)
+  (check-type (access-hashes object) (or null (simple-array fixnum (*)))))
+
+
 (defmethod cl-ds.utils:cloning-information append
-  ((sketch fundamental-data-sketch))
+  ((sketch approximated-counts))
   '((:counters read-counters)
-    (:hashes read-hashes)
-    (:count read-count)
+    (:hashes access-hashes)
+    (:count access-count)
     (:size cl-ds:size)
-    (:hash-fn read-hash-fn)))
+    (:hash-fn access-hash-fn)))
 
 
 (defmethod cl-ds:clone ((sketch approximated-counts))
   (cl-ds.utils:quasi-clone* sketch
     :counters (~> sketch read-counters copy-array)
-    :hashes (~> sketch read-hashes)))
+    :hashes (~> sketch access-hashes)))
 
 
 (defmethod cl-ds:at ((container approximated-counts)
@@ -47,12 +56,12 @@
                                        :format-control "Approximated-counts does not accept more-locations"))
   (iterate
     (with hash = (ldb (byte 32 0)
-                      (funcall (read-hash-fn container)
+                      (funcall (access-hash-fn container)
                                location)))
     (with counts = (read-counters container))
-    (with hashes = (read-hashes container))
-    (with space = (read-space container))
-    (for j from 0 below (read-count container))
+    (with hashes = (access-hashes container))
+    (with space = (access-space container))
+    (for j from 0 below (access-count container))
     (minimize (aref counts (hashval hashes space j hash))
               into min)
     (finally (return (values min t)))))
@@ -81,11 +90,11 @@
   ((element)
    (incf (access-size %data-sketch))
    (iterate
-     (with hash = (funcall (read-hash-fn %data-sketch) element))
-     (with hashes = (read-hashes %data-sketch))
+     (with hash = (funcall (access-hash-fn %data-sketch) element))
+     (with hashes = (access-hashes %data-sketch))
      (with counters = (read-counters %data-sketch))
-     (with count = (read-count %data-sketch))
-     (with space = (read-space %data-sketch))
+     (with count = (access-count %data-sketch))
+     (with space = (access-space %data-sketch))
      (for j from 0 below count)
      (for hashval = (hashval hashes space j hash))
      (incf (aref counters hashval))))
