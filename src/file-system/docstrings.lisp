ebe80b9bc03e73f3251e002b993e83afcd7cf916
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-04-05 15:34:43 +0200

    Added more description to the find function.

--- src/file-system/docstrings.lisp
+++ src/file-system/docstrings.lisp
@@ -6,11 +6,11 @@
 
   (function line-by-line
     (:description "Opens a text file, and allows reading it line by line."
-     :returns "Forward range. Provides access to each line in the file stored as string."
+     :returns "Forward range. Provides access to each line in the file stored as a string."
      :exceptional-situations ("Signalling error from a callback during traverse will close the inner stream automaticly."
                               "Signals the type error if PATH is not (OR STRING PATHNAME)."
                               "Signals errors just as CL:OPEN does.")
      :notes "File is opened lazily. Calling either TRAVERSE or ACROSS on the line-by-line range will automaticly close the inner file. This makes it suitable to use with the aggregation functions without additional code."))
 
   (function find
-    (:description "A function somewhat similar to the posix find tool. Depending on the filesystem content and the DESCRIPTION list, returns a forward-range containing pathnames matching the DESCRIPTION.")))
+    (:description "A function somewhat similar to the posix find tool. Depending on the filesystem content and the DESCRIPTION list, returns a forward-range containing pathnames matching the DESCRIPTION. DESCRIPTION list supports :directory, :regex-file, :regex-directory, :file, :all-directories and :all-files filters. Each of those filters selects matching nodes in the file system.")))
