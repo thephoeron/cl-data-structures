815072a8b380aa5ff18ceb231e9f156e2089980f
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-04-13 11:55:02 +0200

    Updating find manual.

--- src/file-system/docstrings.lisp
+++ src/file-system/docstrings.lisp
@@ -15,5 +15,7 @@
   (function find
     (:description "A function somewhat similar to the posix find tool. Depending on the filesystem content and the DESCRIPTION list, returns a forward-range containing pathnames matching the DESCRIPTION. DESCRIPTION list supports :directory, :regex-file, :regex-directory, :file, :all-directories and :all-files filters. Each of those filters selects matching nodes in the file system and handles it to the next filter."
      :notes ((:directory "This filter is used to specify directory by :PATH.")
-             (:regex-directory "This filter is used to specify directory by PPCRE in the :PATH. Matching directories will be accepted.")
-             (:file "This filter is used to specify file by name.")))))
+             (:regex-directory "This filter is used to specify directory by PPCRE in the :PATH. Directories with a maching name will be accepted.")
+             (:file "This filter is used to specify file by name supplied in :PATH.")
+             (:regex-file "This filter is used to specify file by PPCRE in the :PATH. Files with a matching name will be accepted.")
+             ))))
