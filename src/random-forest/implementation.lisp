c743a32f2bbc62e3c90b6abe3ff93fd8b528c1d0
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-05-17 12:42:12 +0200

    Still working on make-node function.

--- src/random-forest/implementation.lisp
+++ src/random-forest/implementation.lisp
@@ -10,34 +10,33 @@
 (defmethod make-node ((main-model random-forest-classifier)
                       data arguments)
   (bind ((split-attempts (split-attempts main-model))
-         (hash-table (make-hash-table))
-         ((:labels impl (data depth))
+         ((:labels node (data depth))
           (when (zerop depth)
-            (return-from impl nil))
-          (clrhash hash-table)
+            (return-from node nil))
           (bind ((submodel (lret ((r (make-submodel-with-model main-model
                                                                data
                                                                arguments)))
                              (train r data)))
-                 (submodel-predictions (~> (predict main-model data)
-                                           cl-ds.alg:to-vector))
+                 (submodel-predictions (predict main-model data))
                  (data-size (length data))
                  (grouped-predictions (cl-ds.alg:group-by
                                        submodel-predictions
                                        :key #'second))
-                 (gini-impurity (~> (cl-ds.alg:summary
-                                        grouped-predictions
-                                      :count (cl-ds.alg:count-elements)
-                                      :gini (cl-ds.math:gini-impurity
-                                             submodel-predictions
-                                             :hash-table hash-table
-                                             :key #'cadar))
-                                    (cl-ds.alg:accumulate
-                                     #'+
-                                     :initial-value 0.0
-                                     :key (lambda (x)
-                                            (* (cl-ds:at x :count)
-                                               (/ (cl-ds:at x :gini)
-                                                  data-size)))))))
+                 (summary (cl-ds.alg:summary
+                              grouped-predictions
+                            :count (cl-ds.alg:count-elements)
+                            :content (cl-ds.alg:to-vector :key #'first)
+                            :gini (cl-ds.math:gini-impurity
+                                   submodel-predictions
+                                   :key #'cadar)))
+                 (classes-count (cl-ds:size summary))
+                 (gini-impurity (cl-ds.alg:accumulate
+                                 summary
+                                 #'+
+                                 :initial-value 0.0
+                                 :key (lambda (group &aux (x (cdr group)))
+                                        (* (cl-ds:at x :count)
+                                           (/ (cl-ds:at x :gini)
+                                              data-size))))))
 
             )))))
