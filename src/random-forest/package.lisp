c3a190b1bcd3c68e6a0992dd1b0b328dc75879cb
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-05-24 11:23:36 +0200

    Export protocol function names for the RF.

--- src/random-forest/package.lisp
+++ src/random-forest/package.lisp
@@ -6,4 +6,22 @@
         #:cl-data-structures.aux-package)
   (:nicknames #:cl-ds.rf)
   (:export
+   #:encode-data-into-context
+   #:encode-data-into-contexts
+   #:make-model
+   #:make-node
+   #:make-submodel
+   #:make-submodel-prediction-context
+   #:make-submodel-prediction-contexts
+   #:make-submodel-with-model
+   #:predict
+   #:split-attempts
+   #:submodel-arguments
+   #:submodel-class
+   #:submodel-predict
+   #:submodels
+   #:train
+   #:tree-count
+   #:tree-maximum-depth
+   #:tree-minimal-size
    ))
