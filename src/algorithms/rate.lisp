8ec9b56ae5c027fcf02b5039c1cd7ffa2fefa974
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-11-06 10:21:06 +0100

    Added key arguments positive and total to the rate function.

--- src/algorithms/rate.lisp
+++ src/algorithms/rate.lisp
@@ -3,16 +3,16 @@
 
 (cl-ds.alg.meta:define-aggregation-function
     rate rate-function
-    (:range test &key key)
-    (:range test &key (key #'identity))
+    (:range test &key key positive total)
+    (:range test &key (key #'identity) (positive 0) (total 0))
 
     (%positive %total %test)
 
-    ((&key test &allow-other-keys)
+    ((&key test positive total &allow-other-keys)
      (ensure-functionf test)
-     (setf %positive 0
+     (setf %positive positive
            %test test
-           %total 0))
+           %total total))
 
     ((element)
      (incf %total)
