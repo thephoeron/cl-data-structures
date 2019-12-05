af097552f58ada3771c9e85508ce08bbef336325
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-12-05 16:30:46 +0100

    Added locate-node function stub.

--- src/common/skip-list/common.lisp
+++ src/common/skip-list/common.lisp
@@ -4,3 +4,8 @@
 (defstruct skip-list-node
   (rests #() :type simple-vector)
   (content nil :type t))
+
+
+(-> locate-node ((or null skip-list-node) t) simple-array)
+(defun locate-node (skip-list-node item)
+  cl-ds.utils:todo)
