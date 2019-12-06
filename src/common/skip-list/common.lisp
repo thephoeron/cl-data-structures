25f098429e1abf048d26ddbdcf9f037af9d7a03d
Marek Kochanowicz <sirherrbatka@gmail.com>
2019-12-06 21:13:18 +0100

    Handle corner case when the first element is already greater then searched.

--- src/common/skip-list/common.lisp
+++ src/common/skip-list/common.lisp
@@ -13,20 +13,52 @@
 
 (-> locate-node (simple-vector t function) (or null skip-list-node))
 (defun locate-node (pointers item test)
-  (iterate
-    (declare (type fixnum i)
-             (type simple-vector result))
-    (with result = pointers)
-    (with result-node = nil)
-    (with i = (~> pointers length 1-))
-    (for node = (aref result i))
-    (if (and node
-             (funcall test
-                      (skip-list-node-content node)
-                      item))
-        (setf result (skip-list-node-pointers node)
-              i (~> result length 1-)
-              result-node (aref result i))
-        (decf i))
-    (while (>= i 0))
-    (finally (return result-node))))
+  (let* ((pointers-length (~> pointers length))
+         (last (1- pointers-length)))
+    (iterate
+      (declare (type fixnum i))
+      (for i from last downto 0)
+      (for node = (aref pointers i))
+      (when (null node)
+        (next-iteration))
+      (for content = (skip-list-node-content node))
+      (when (funcall test item content)
+        (return-from locate-node node)))
+    (iterate
+      (declare (type fixnum i)
+               (type simple-vector result))
+      (with result = pointers)
+      (with result-node = nil)
+      (with i = last)
+      (for node = (aref result i))
+      (if (and node
+               (funcall test
+                        (skip-list-node-content node)
+                        item))
+          (setf result (skip-list-node-pointers node)
+                i (~> result length 1-)
+                result-node (aref result i))
+          (decf i))
+      (while (>= i 0))
+      (finally (return result-node)))))
+
+
+(defclass fundamental-skip-list ()
+  ((%head :initarg :head
+          :reader read-head)
+   (%tail :initarg :tail
+          :accessor access-tail)
+   (%maximum-level :initarg :maximum-level
+                   :accessor access-maximum-level)))
+
+
+(cl-ds.utils:define-list-of-slots fundamental-skip-list ()
+  (head read-head)
+  (tail access-tail)
+  (maximum-level access-maximum-level))
+
+
+(defmethod cl-ds.utils:cloning-information append ((object fundamental-skip-list))
+  '((:head read-head)
+    (:tail access-tail)
+    (:maximum-level access-maximum-level)))
--- src/common/skip-list/tests.lisp
+++ src/common/skip-list/tests.lisp
@@ -2,7 +2,7 @@
 (defpackage skip-list-tests (:use :cl :cl-data-structures.aux-package))
 (in-package #:skip-list-tests)
 
-(prove:plan 2)
+(prove:plan 3)
 
 (let* ((node1 (cl-ds.common.skip-list:make-skip-list-node
                :pointers (vector nil)
@@ -33,5 +33,20 @@
                5
                #'<)))
   (prove:is found nil))
+
+(let* ((node1 (cl-ds.common.skip-list:make-skip-list-node
+               :pointers (vector nil)
+               :content 3))
+       (node2 (cl-ds.common.skip-list:make-skip-list-node
+               :pointers (vector node1)
+               :content 2))
+       (node3 (cl-ds.common.skip-list:make-skip-list-node
+               :pointers (vector node2)
+               :content 1))
+       (found (cl-ds.common.skip-list:locate-node
+               (vector node3)
+               0
+               #'<)))
+  (prove:is found node3))
 
 (prove:finalize)
