ff28aa22ae9dca05d8f14c37f8e983086945de01
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2020-01-15 13:45:52 +0100

    Replaced clara aggregation functions with functions that work in a single stage.

--- src/clustering/clara.lisp
+++ src/clustering/clara.lisp
@@ -1,102 +1,82 @@
 (cl:in-package #:cl-data-structures.clustering)
 
+(cl-ds.alg.meta:define-aggregation-function
+    clara-variable-number-of-medoids
+    clara-variable-number-of-medoids-function
 
-(defclass clara-function (cl-ds.alg.meta:multi-aggregation-function)
-  ()
-  (:metaclass closer-mop:funcallable-standard-class))
+    (:range from to sample-size
+     sample-count metric-fn
+     &key key select-medoids-attempts-count
+     attempts  split merge)
 
+    (:range from to sample-size
+     sample-count metric-fn
+     &key (key #'identity) (select-medoids-attempts-count 50)
+     (attempts 0) split merge)
 
-(defclass clara-variable-number-of-medoids (clara-function)
-  ()
-  (:metaclass closer-mop:funcallable-standard-class))
+    (%data %from %to %sample-size %sample-count %metric-fn
+     %select-medoids-attempts-count %attempts %split %merge)
 
+    ((&key from to sample-size sample-count
+           metric-fn select-medoids-attempts-count
+           attempts split merge
+           &allow-other-keys)
+     (setf %data (vect)
+           %from from
+           %to to
+           %sample-size sample-size
+           %sample-count sample-count
+           %metric-fn metric-fn
+           %select-medoids-attempts-count select-medoids-attempts-count
+           %attempts attempts
+           %split split
+           %merge merge))
 
-(defgeneric clara (range number-of-medoids sample-size
-                   sample-count metric-fn
-                   &key
-                     key select-medoids-attempts-count
-                     attempts split merge)
-  (:generic-function-class clara-function)
-  (:method (range number-of-medoids sample-size
-            sample-count metric-fn &key
-              (key #'identity) (select-medoids-attempts-count 50)
-              (attempts 0) split merge)
-    (cl-ds.alg.meta:apply-range-function
-     range #'clara
-     :key key
-     :number-of-medoids number-of-medoids
-     :sample-size sample-size
-     :sample-count sample-count
-     :metric-fn metric-fn
-     :select-medoids-attempts-count select-medoids-attempts-count
-     :attempts attempts
-     :split split
-     :merge merge)))
+    ((element)
+     (vector-push-extend element %data))
 
-
-(defgeneric clara-variable-number-of-medoids (range from to sample-size
-                                              sample-count metric-fn
-                                              &key
-                                                key select-medoids-attempts-count
-                                                attempts split merge)
-  (:generic-function-class clara-variable-number-of-medoids)
-  (:method (range from to sample-size
-            sample-count metric-fn
-            &key (key #'identity) (select-medoids-attempts-count 50)
-              (attempts 0) split merge)
-    (cl-ds.alg.meta:apply-range-function
-     range #'clara-variable-number-of-medoids
-     :key key
-     :from from
-     :to to
-     :sample-size sample-size
-     :sample-count sample-count
-     :metric-fn metric-fn
-     :select-medoids-attempts-count select-medoids-attempts-count
-     :attempts attempts
-     :split split
-     :merge merge)))
+    ((cl-ds.utils.cluster.clara/pam:clara-variable-number-of-medoids
+      %data
+      %metric-fn %sample-size %sample-count %from %to
+      :select-medoids-attempts-count %select-medoids-attempts-count
+      :attempts %attempts :split %split :merge %merge)))
 
 
-(defmethod cl-ds.alg.meta:multi-aggregation-stages
-    ((function clara-function)
-     &rest all
-     &key
-       sample-size sample-count metric-fn number-of-medoids
-       key select-medoids-attempts-count attempts split merge
-     &allow-other-keys)
-  (declare (ignore all))
-  (list (cl-ds.alg.meta:stage :vector (range &rest all)
-          (declare (ignore all))
-          (cl-ds.alg:to-vector range :force-copy nil))
+(cl-ds.alg.meta:define-aggregation-function
+    clara
+    clara-function
 
-        (lambda (&key vector &allow-other-keys)
-          (declare (type vector vector))
-          (cl-ds.utils.cluster.clara/pam:clara
-           vector
-           number-of-medoids metric-fn sample-size sample-count
-           :key key
-           :select-medoids-attempts-count select-medoids-attempts-count
-           :attempts attempts :split split :merge merge))))
+    (:range number-of-medoids sample-size
+     sample-count metric-fn
+     &key key select-medoids-attempts-count
+     attempts split merge)
 
+    (:range number-of-medoids sample-size sample-count metric-fn
+     &key (key #'identity) (select-medoids-attempts-count 50)
+     (attempts 0) split merge)
 
-(defmethod cl-ds.alg.meta:multi-aggregation-stages
-    ((function clara-variable-number-of-medoids)
-     &rest all
-     &key
-       from to sample-size sample-count metric-fn
-       key select-medoids-attempts-count attempts split merge
-     &allow-other-keys)
-  (declare (ignore all))
-  (list (cl-ds.alg.meta:stage :vector (range &rest all)
-          (declare (ignore all))
-          (cl-ds.alg:to-vector range :force-copy nil))
+    (%data %number-of-medoids %sample-size %sample-count %metric-fn
+     %select-medoids-attempts-count %attempts %split %merge)
 
-        (lambda (&key vector &allow-other-keys)
-          (declare (type vector vector))
-          (cl-ds.utils.cluster.clara/pam:clara-variable-number-of-medoids
-           vector
-           metric-fn sample-size sample-count from to
-           :key key
-           :select-medoids-attempts-count select-medoids-attempts-count
-           :attempts attempts :split split :merge merge))))
+    ((&key number-of-medoids sample-size sample-count
+           metric-fn select-medoids-attempts-count
+           attempts split merge
+           &allow-other-keys)
+     (setf %data (vect)
+           %number-of-medoids number-of-medoids
+           %sample-size sample-size
+           %sample-count sample-count
+           %metric-fn metric-fn
+           %select-medoids-attempts-count select-medoids-attempts-count
+           %attempts attempts
+           %split split
+           %merge merge))
+
+    ((element)
+     (vector-push-extend element %data))
+
+    ((cl-ds.utils.cluster.clara/pam:clara
+      %data
+      %number-of-medoids %metric-fn %sample-size %sample-count
+      :select-medoids-attempts-count %select-medoids-attempts-count
+      :attempts %attempts :split %split :merge %merge)))
