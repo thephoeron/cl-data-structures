7fb890c70185de0a0b21bcf51eb2c50ff879645b
Marek Kochanowicz <sirherrbatka@gmail.com>
2020-01-15 19:07:04 +0100

    Replace k-means with a single stage aggregation.

--- src/clustering/k-means.lisp
+++ src/clustering/k-means.lisp
@@ -1,58 +1,46 @@
 (in-package #:cl-data-structures.clustering)
 
 
-(defclass k-means-function (cl-ds.alg.meta:multi-aggregation-function)
-  ()
-  (:metaclass closer-mop:funcallable-standard-class))
+(cl-ds.alg.meta:define-aggregation-function
+    k-means k-means-function
 
+    (:range number-of-medoids distortion-epsilon
+     &key key silhouette-sample-size iterations silhouette-sample-count)
 
-(defgeneric k-means (range number-of-medoids distortion-epsilon
-                     &rest all
-                     &key key silhouette-sample-size iterations
-                       silhouette-sample-count)
-  (:generic-function-class k-means-function)
-  (:method (range number-of-medoids distortion-epsilon
-            &rest all
-            &key (key #'identity)
-              (silhouette-sample-size 500)
-              (iterations nil)
-              (silhouette-sample-count 15))
-    (declare (ignore silhouette-sample-size iterations
-                     silhouette-sample-count key))
-    (apply #'cl-ds.alg.meta:apply-range-function
-           range #'k-means
-           :number-of-medoids number-of-medoids
-           :distortion-epsilon distortion-epsilon
-           all)))
+    (:range number-of-medoids distortion-epsilon
+     &key (key 'identity) (silhouette-sample-size 500)
+     (iterations nil) (silhouette-sample-count 15))
 
+    (%number-of-medoids %distortion-epsilon %silhouette
+     %silhouette-sample-size %iterations %silhouette-sample-count
+     %data)
 
-(defmethod cl-ds.alg.meta:multi-aggregation-function
-    ((function k-means-function)
-     &rest all
-     &key number-of-medoids distortion-epsilon
-       silhouette-sample-size iterations
-       silhouette-sample-count)
-  (check-type number-of-medoids integer)
-  (check-type distortion-epsilon single-float)
-  (check-type silhouette-sample-size integer)
-  (check-type silhouette-sample-count integer)
-  (check-type iterations (or null integer))
-  (cl-ds:check-argument-bounds distortion-epsilon
-                               (< 0.0 distortion-epsilon))
-  (cl-ds:check-argument-bounds silhouette-sample-size
-                               (< 0 silhouette-sample-size))
-  (cl-ds:check-argument-bounds silhouette-sample-count
-                               (< 0 silhouette-sample-count))
-  (cl-ds:check-argument-bounds number-of-medoids
-                               (< 0 number-of-medoids))
-  (unless (null iterations)
-    (cl-ds:check-argument-bounds iterations
-                                 (< 0 iterations)))
-  (list (cl-ds.alg.meta:stage :vector (range &rest all)
-          (declare (ignore all))
-          (cl-ds.alg:to-vector range :force-copy nil))
+    ((&key number-of-medoids distortion-epsilon silhouette silhouette-sample-size
+           iterations silhouette-sample-count &allow-other-keys)
+     (cl-ds:check-argument-bounds distortion-epsilon
+                                  (< 0.0 distortion-epsilon))
+     (cl-ds:check-argument-bounds silhouette-sample-size
+                                  (< 0 silhouette-sample-size))
+     (cl-ds:check-argument-bounds silhouette-sample-count
+                                  (< 0 silhouette-sample-count))
+     (cl-ds:check-argument-bounds number-of-medoids
+                                  (< 0 number-of-medoids))
+     (unless (null iterations)
+       (cl-ds:check-argument-bounds iterations
+                                    (< 0 iterations)))
+     (setf %number-of-medoids       number-of-medoids
+           %distortion-epsilon      distortion-epsilon
+           %silhouette              silhouette
+           %silhouette-sample-size  silhouette-sample-size
+           %iterations              iterations
+           %data                    (vect)
+           %silhouette-sample-count silhouette-sample-count))
 
-        (lambda (&key vector &allow-other-keys)
-          (apply #'cl-ds.utils.cluster.k-means:k-means
-                 vector number-of-medoids distortion-epsilon
-                 (cddr all)))))
+    ((element)
+     (vector-push-extend element %data))
+
+    ((cl-ds.utils.cluster.k-means:k-means
+      %data %number-of-medoids %distortion-epsilon
+      :silhouette-sample-size  %silhouette-sample-size
+      :iterations              %iterations
+      :silhouette-sample-count %silhouette-sample-count)))
