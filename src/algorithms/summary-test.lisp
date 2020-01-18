6370569c9da2ef605f2be2ce7820b7c2b3af2b52
Marek Kochanowicz <sirherrbatka@gmail.com>
2020-01-18 23:38:43 +0100

    Pass nil to construct-aggregator in the summary.

--- src/algorithms/summary-test.lisp
+++ src/algorithms/summary-test.lisp
@@ -9,7 +9,7 @@
 (let* ((data (cl-ds:xpr (:i 0)
                (when (< i 250)
                  (cl-ds:send-recur i :i (1+ i)))))
-       (min-and-max (cl-ds.alg:summary data
+       (min-and-max (cl-ds.alg:summary (cl-ds.alg:group-by data :key #'evenp)
                       :min (cl-ds.alg:accumulate #'min)
                       :max (cl-ds.alg:accumulate #'max))))
   (is (cl-ds:at min-and-max :min) 0)
