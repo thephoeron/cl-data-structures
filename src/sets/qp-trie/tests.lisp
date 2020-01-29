7135db82a4967caab32309f582ed87e7e157f62a
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2020-01-29 13:30:54 +0100

    Added failing test for vector.

--- src/sets/qp-trie/tests.lisp
+++ src/sets/qp-trie/tests.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:cl-data-structures.sets.qp-trie)
 
-(prove:plan 501)
+(prove:plan 502)
 
 (let ((trie (make-mutable-qp-trie-set))
       (data (make-array 500)))
@@ -14,6 +14,17 @@
     (prove:ok (cl-ds:at trie vect)))
   (prove:is (cl-ds.alg:count-elements trie)
             (~> data (remove-duplicates :test #'vector=) length))
-  )
+  (prove:is (cl-ds:size trie) (cl-ds.alg:count-elements trie))
+  (prove:is (~> trie cl-ds:whole-range cl-ds.alg:to-vector
+                (sort #'> :key (rcurry #'aref 0))
+                (stable-sort #'> :key (rcurry #'aref 1))
+                (stable-sort #'> :key (rcurry #'aref 2))
+                (stable-sort #'> :key (rcurry #'aref 3)))
+            (~> data
+                (sort #'> :key (rcurry #'aref 0))
+                (stable-sort #'> :key (rcurry #'aref 1))
+                (stable-sort #'> :key (rcurry #'aref 2))
+                (stable-sort #'> :key (rcurry #'aref 3)))
+            :test #'equalp))
 
 (prove:finalize)
