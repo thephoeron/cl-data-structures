72592b437e11c6f3161c9b009344e51733aee0d3
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2020-02-03 17:07:05 +0100

    Corrected enumerate implementation.

--- src/algorithms/enumerate.lisp
+++ src/algorithms/enumerate.lisp
@@ -10,13 +10,12 @@
           (key #'identity)
           (size 16))
 
-  (%table %hash-table-key %number)
+  (%table %number)
 
   ((setf %table (make-hash-table :test test :size size)))
 
   ((element)
-   (let ((key (funcall %hash-table-key element)))
-     (ensure (gethash key %table)
-       (prog1 %number (incf %number)))))
+   (ensure (gethash element %table)
+     (prog1 %number (incf %number))))
 
   (%table))
