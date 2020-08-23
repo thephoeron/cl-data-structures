a03c6cdb9e26f7ead6cbbca2a5bff1b4e5bd049d
Marek Kochanowicz <sirherrbatka@gmail.com>
2020-08-23 14:32:32 +0200

    Rewrite of the bourgain embbeddings function.

--- src/utils/embedding.lisp
+++ src/utils/embedding.lisp
@@ -1,46 +1,35 @@
 (cl:in-package #:cl-ds.utils)
 
 
-(defun bourgain-embedding (vector-of-elements c distance-fn
-                           &key
-                             (embedding-type 'single-float) (parallel t)
-                             (m2 (ceiling (log (length vector-of-elements)))))
-  (declare (type vector vector-of-elements)
-           (type fixnum m2)
-           (optimize (speed 3))
-           (type positive-fixnum c)
-           (type boolean parallel))
+(defun bourgain-embedding (vector-of-elements distance-fn &key (metric-type 'single-float))
+  (declare (type vector vector-of-elements))
   (ensure-functionf distance-fn)
-  (check-type vector-of-elements vector)
-  (nest
-   (cases ((simple-vector-p vector-of-elements)
-           (array-has-fill-pointer-p vector-of-elements)
-           parallel))
-   (let* ((length (length vector-of-elements))
-          (embeddings (make-array length))
-          (m1 (ceiling (1- (/ m2 (log 2)))))
-          (element-type (array-element-type vector-of-elements)))
-     (declare (type fixnum length m1 m2)
-              (type simple-vector embeddings)))
-   (bind ((sample (make-array (expt 2 m1)
-                              :fill-pointer 0
-                              :element-type element-type)))
-     (map-into embeddings (curry #'make-array (the fixnum (* m1 m2))
-                                 :element-type embedding-type
-                                 :fill-pointer 0))
-     (iterate
-       (declare (type fixnum i))
-       (for i from 1 to m1)
-       (iterate
-         (declare (type fixnum j))
-         (for j from 1 to m2)
-         (draw-sample-vector vector-of-elements (expt 2 i) sample)
-         (funcall (if parallel #'lparallel:pmap #'map) nil
-                  (lambda (embedding x)
-                    (iterate
-                      (for other-x in-vector sample)
-                      (minimize (funcall distance-fn x other-x) into distance)
-                      (finally (vector-push-extend distance embedding))))
-                  embeddings
-                  vector-of-elements)))
-     embeddings)))
+  (bind ((length (length vector-of-elements))
+         (m1 (ceiling (1- (/ (log length) (log 2)))))
+         (m2 (ceiling (log length)))
+         (embeddings (make-array (list length (* m1 m2))
+                                 :element-type metric-type))
+         (embeddings-view (make-array (list length m1 m2)
+                                      :element-type metric-type
+                                      :displaced-to embeddings))
+         ((:flet random-sample (count))
+          (iterate
+            (with result = (make-array count))
+            (for i from 0 below count)
+            (setf (aref result i)
+                  (aref vector-of-elements (random length)))
+            (finally (return result)))))
+    (declare (type fixnum length m1 m2)
+             (type (simple-array * (* *)) embeddings))
+    (iterate
+      (for i from 0 below m1)
+      (iterate
+        (for j from 0 below m2)
+        (for s = (random-sample (expt 2 i)))
+        (iterate
+          (for k from 0 below length)
+          (for x = (aref vector-of-elements k))
+          (for distance = (reduce #'min s
+                                  :key (curry distance-fn x)))
+          (setf (aref embeddings-view k i j) distance))))
+    embeddings))
